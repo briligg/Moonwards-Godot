e52fcee81a2c476fb05a4dc5b388b613b2483c51
Gyrth <gyrthmcmulin@gmail.com>
2019-09-18 03:18:15 +0200

    Restyled avatar preview script in the player settings.

--- scripts/AvatarPreview.gd
+++ scripts/AvatarPreview.gd
@@ -1,51 +1,52 @@
 extends Spatial
 
-export(SpatialMaterial) var base_material
+export(SpatialMaterial) var base_material : SpatialMaterial
 
-var pants_mat
-var shirt_mat
-var skin_mat
-var hair_mat
-var shoes_mat
-var selected 
-var selected_mat = load("res://assets/UI/Options/selected.material")
-var unselected_mat = load("res://assets/UI/Options/unselected.material")
-func _ready():
-	pants_mat = base_material.duplicate()
-	shirt_mat = base_material.duplicate()
-	skin_mat = base_material.duplicate()
-	hair_mat = base_material.duplicate()
-	shoes_mat = base_material.duplicate()
+var _pants_mat : SpatialMaterial
+var _shirt_mat : SpatialMaterial
+var _skin_mat : SpatialMaterial
+var _hair_mat : SpatialMaterial
+var _shoes_mat : SpatialMaterial
+var _selected_mat : SpatialMaterial = load("res://assets/UI/Options/selected.material")
+var _unselected_mat : SpatialMaterial = load("res://assets/UI/Options/unselected.material")
+
+func _ready() -> void:
+	_pants_mat = base_material.duplicate()
+	_shirt_mat = base_material.duplicate()
+	_skin_mat = base_material.duplicate()
+	_hair_mat = base_material.duplicate()
+	_shoes_mat = base_material.duplicate()
 	
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(1, pants_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(0, shirt_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(2, skin_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(4, hair_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(3, shoes_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(1, _pants_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(0, _shirt_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(2, _skin_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(4, _hair_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(3, _shoes_mat)
 	
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(2, pants_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(3, shirt_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(4, skin_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(1, hair_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(0, shoes_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(2, _pants_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(3, _shirt_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(4, _skin_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(1, _hair_mat)
+	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(0, _shoes_mat)
 	
-func clean_selected():
+func clean_selected() -> void:
 	for genders in $AvatarRig/FemaleRig/Skeleton.get_children():
 		for selector in genders.get_children():
-			selector.set_surface_material(0, unselected_mat)
-func set_selected(idx):
+			selector.set_surface_material(0, _unselected_mat)
+
+func set_selected(idx : int) -> void:
 	for genders in $AvatarRig/FemaleRig/Skeleton.get_children():
-		genders.get_child(idx).set_surface_material(0,selected_mat)
+		genders.get_child(idx).set_surface_material(0, _selected_mat)
 
-func SetColors(var pants, var shirt, var skin, var hair, var shoes):
-	pants_mat.albedo_color = pants
-	shirt_mat.albedo_color = shirt
-	skin_mat.albedo_color = skin
-	hair_mat.albedo_color = hair
-	shoes_mat.albedo_color = shoes
+func set_colors(pants : Color, shirt : Color, skin : Color, hair : Color, shoes : Color) -> void:
+	_pants_mat.albedo_color = pants
+	_shirt_mat.albedo_color = shirt
+	_skin_mat.albedo_color = skin
+	_hair_mat.albedo_color = hair
+	_shoes_mat.albedo_color = shoes
 
-func SetGender(var gender):
-	if int(gender) == 0:
+func set_gender(gender : int) -> void:
+	if gender == 0:
 		$AvatarRig/FemaleRig/Skeleton/AvatarFemale.show()
 		$AvatarRig/FemaleRig/Skeleton/AvatarMale.hide()
 	else:
