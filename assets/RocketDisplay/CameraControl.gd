44bcc79d11d2e4e9462b6ff0e97ebc8eecf1d823
Gyrth <gyrthmcmulin@gmail.com>
2019-09-20 17:51:10 +0200

    Improved camera control by enabling and disabling the process and input.

--- assets/RocketDisplay/CameraControl.gd
+++ assets/RocketDisplay/CameraControl.gd
@@ -12,15 +12,15 @@ const MAX_DOWN_AIM_ANGLE : float = 55.0
 onready var _camera_position : Node = $CameraPosition
 
 var _current_zoom_distance : float = 20.0
-var _enabled : bool = false
 var _target_rotation_degrees : Vector3 = Vector3()
 var _look_direction : Vector2 = Vector2()
 var _mouse_down : bool = false
 
-func _physics_process(delta : float) -> void:
-	if not _enabled:
-		return
+func _ready():
+	set_physics_process(false)
+	set_process_input(false)
 
+func _physics_process(delta : float) -> void:
 	_camera_position.translation = _camera_position.translation.linear_interpolate(Vector3(0, 0, _current_zoom_distance), delta * speed)
 	rotation_degrees = rotation_degrees.linear_interpolate(_target_rotation_degrees, delta * speed)
 
@@ -33,18 +33,17 @@ func _physics_process(delta : float) -> void:
 		rotation_degrees.y = -180
 		_target_rotation_degrees.y -= 360
 
-func set_enabled(new_enabled : bool) -> void:
-	if new_enabled:
+func set_enabled(enabled : bool) -> void:
+	if enabled:
 		_current_zoom_distance = _camera_position.translation.z
 		_look_direction.y = rotation_degrees.x
 		_look_direction.x = rotation_degrees.y
 		_target_rotation_degrees = Vector3(_look_direction.y, _look_direction.x, 0)
-
-	_enabled = new_enabled
+	
+	set_physics_process(enabled)
+	set_process_input(enabled)
 
 func _input(event : InputEvent) -> void:
-	if not _enabled:
-		return
 	if event.is_action("scroll_down"):
 		increase_distance()
 	if event.is_action("scroll_up"):
@@ -67,13 +66,9 @@ func _input(event : InputEvent) -> void:
 		_target_rotation_degrees = Vector3(_look_direction.y, _look_direction.x, 0)
 
 func increase_distance() -> void:
-	if not _enabled:
-		return
 	if _current_zoom_distance < max_zoom_distance:
 		_current_zoom_distance += zoom_step_size
 
 func decrease_distance() -> void:
-	if not _enabled:
-		return
 	if _current_zoom_distance > min_zoom_distance:
 		_current_zoom_distance -= zoom_step_size
