53c3493c91a01b2f1bdab2215539e342b70f9b33
Zach777 <MeteorCity3109@protonmail.com>
2020-01-10 22:04:28 -0500

    Set up Lobby to allow player name changes.

--- Interface/MainMenu/Lobby/Lobby.gd
+++ Interface/MainMenu/Lobby/Lobby.gd
@@ -9,11 +9,14 @@ enum STATE {
 
 onready var ServerWait = $WaitServer/Label.text
 
+#The input field for choosing the player's name.
+onready var player_name_box : LineEdit = get_node( "connect/namecontainer/name_input" )
+
 var state = STATE.INIT setget set_state
 var binddef : Dictionary = { src = null, dest = null }
 
 func _ready() -> void:
-	set_name(Options.username)
+	set_name_box(Options.username)
 
 func bind_to_node_utilities() -> void :
 	#Bind myself to the node utilities so that I can listen to errors and reports.
@@ -24,7 +27,7 @@ func bind_to_node_utilities() -> void :
 func is_valid_connection() -> bool :
 	#When trying to connect, check that everything is as it should be.
 	if not (GameState.RoleServer or GameState.RoleClient):
-		if ($connect/name.text == ""):
+		if (player_name_box.text == ""):
 			$connect/error_label.text="Invalid name!"
 			return false
 	
@@ -37,16 +40,20 @@ func is_valid_connection() -> bool :
 func register_player_local() -> void :
 	#Register the player with the game state.
 	var player_data = {
-		username = $connect/name.text,
+		username = player_name_box.text,
 		gender = Options.gender,
 		colors = {"pants" : Options.pants_color, "shirt" : Options.shirt_color, "skin" : Options.skin_color, "hair" : Options.hair_color, "shoes" : Options.shoes_color}
 	}
 	GameState.player_register(player_data, true) #local player
 
-func set_name(name : String = "") -> void:
+func save_name( new_name : String ) -> void :
+	#Called whenever the player inputs text in the name box.
+	Options.username = new_name
+
+func set_name_box(name : String = "") -> void:
 	if name == "":
 		name = Utilities.get_name()
-	$connect/name.text = name
+	player_name_box.text = name
 
 func state_hide() -> void:
 	match state:
@@ -160,7 +167,7 @@ func _on_Sinlgeplayer_pressed() -> void:
 		get_node("connect/error_label").text="Invalid name!"
 		return
 	var player_data = {
-		username = $connect/name.text,
+		username = player_name_box.text,
 		network = false
 	}
 	GameState.RoleNoNetwork = true
@@ -169,7 +176,3 @@ func _on_Sinlgeplayer_pressed() -> void:
 	yield(get_tree().create_timer(2), "timeout")
 	state_hide()
 	GameState.change_scene(worldscene)
-
-func _on_Button2_pressed() -> void:
-	set_name()
-	yield(get_tree().create_timer(0.1), "timeout")
