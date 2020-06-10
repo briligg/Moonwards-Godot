90f1d756aa445f2c6b8a8ed00b8a88e8a165194a
Zach777 <MeteorCity3109@protonmail.com>
2020-06-10 16:02:35 -0400

    Add filter log messages ability

--- SceneComponent/Interface/LoggerConsole/LoggerConsole.gd
+++ SceneComponent/Interface/LoggerConsole/LoggerConsole.gd
@@ -10,3 +10,12 @@ func _input(event: InputEvent) -> void:
 		var visible_status : bool = !get_child(0).visible
 		for child in get_children() :
 			child.visible = visible_status
+
+
+func _filter_toggled(_button_pressed):
+	var trace = get_node("Container/Filters/Trace").pressed
+	var debug = get_node("Container/Filters/Debug").pressed
+	var warning =  get_node("Container/Filters/Warning").pressed
+	var error =  get_node("Container/Filters/Error").pressed
+	var critical =  get_node("Container/Filters/Critical").pressed
+	get_node("Container/LoggerText").filter_text(trace, debug, warning, error, critical)
--- SceneComponent/Interface/LoggerConsole/LoggerText.gd
+++ SceneComponent/Interface/LoggerConsole/LoggerText.gd
@@ -1,11 +1,20 @@
 extends RichTextLabel
 
 
-var unfiltered_text : String = ""
+const TRACE = 0
+const DEBUG = 1
+const WARNING = 2
+const ERROR = 4
+const CRITICAL = 8
 
-func _add_text(message : String) -> void :
-	unfiltered_text += "\n"
-	unfiltered_text += message
+
+var trace_text : PoolStringArray = PoolStringArray()
+var debug_text : PoolStringArray = PoolStringArray()
+var warning_text : PoolStringArray = PoolStringArray()
+var error_text : PoolStringArray = PoolStringArray()
+var critical_text : PoolStringArray = PoolStringArray()
+
+var message_type_history : Array = []
 
 func _filter_text(message : String) -> String :
 	message = message.right(message.find("]", 0) + 1)
@@ -18,30 +27,35 @@ func _on_trace_logged(message) -> void:
 	message = _filter_text(message)
 	bbcode_text += "\n" # new_line uses buggy append_bbcode func
 	bbcode_text += message
-	_add_text(message)
+	trace_text.append("\n" + message)
+	message_type_history.append(TRACE)
 
 func _on_debug_logged(message) -> void:
 	message = _filter_text(message)
 	bbcode_text += "\n" # new_line uses buggy append_bbcode func
 	bbcode_text += "[color=#03fc8c]" + message + "[/color]"
-	_add_text("[color=#03fc8c]" + message + "[/color]")
+	debug_text.append("\n" + message)
+	message_type_history.append(DEBUG)
 
 func _on_warning_logged(message) -> void:
 	bbcode_text += "\n" # new_line uses buggy append_bbcode func
 	bbcode_text += "[color=yellow]" + message + "[/color]"
-	_add_text("[color=yellow]" + message + "[/color]")
+	warning_text.append("\n" + message)
+	message_type_history.append(WARNING)
 
 func _on_error_logged(message) -> void:
 	message = _filter_text(message)
 	bbcode_text += "\n" # new_line uses buggy append_bbcode func
 	bbcode_text += "[color=#fc5603]" + message + "[/color]"
-	_add_text("[color=#fc5603]" + message + "[/color]")
+	error_text.append("\n" + message)
+	message_type_history.append(ERROR)
 
 func _on_critical_logged(message) -> void:
 	message = _filter_text(message)
 	bbcode_text += "\n" # new_line uses buggy append_bbcode func
 	bbcode_text += "[color=red]" + message + "[/color]"
-	_add_text("[color=red]" + message + "[/color]")
+	critical_text.append("\n" + message)
+	message_type_history.append(CRITICAL)
 
 func deferred_ready() -> void :
 	var logger : Node = get_parent().get_parent().get_parent()
@@ -58,6 +72,45 @@ func deferred_ready() -> void :
 #Filter out messages from levels you do not care about.
 func filter_text(trace : bool, debug : bool, warning : bool,
 					error : bool, critical : bool) -> void :
-	pass
+	bbcode_text = ""
+					
+	#Setup an Array for determine which messages to
+	#let through.
+	var unfiltered_message_types : Array = []
+	if trace: unfiltered_message_types.append(TRACE)
+	if debug: unfiltered_message_types.append(DEBUG)
+	if warning: unfiltered_message_types.append(WARNING)
+	if error: unfiltered_message_types.append(ERROR)
+	if critical: unfiltered_message_types.append(CRITICAL)
+	
+	#These are for placing in the relevant unfiltered messages.
+	var trace_at : int = 0
+	var debug_at : int = 0
+	var warning_at : int = 0
+	var error_at : int = 0
+	var critical_at : int = 0
+	
+	#Go through history and check if the message should be filtered
+	#or not.
+	for type in message_type_history :
+		if unfiltered_message_types.has(type) :
+			
+			#We are an allowed message. Add to LoggerText output
+			if type == TRACE && not trace_text.empty() :
+				bbcode_text += trace_text[trace_at]
+				trace_at += 1
+			elif type == DEBUG && not debug_text.empty() :
+				bbcode_text += debug_text[debug_at]
+				debug_at += 1
+			elif type == WARNING && not warning_text.empty() :
+				bbcode_text += warning_text[warning_at]
+				warning_at += 1
+			elif type == ERROR && not error_text.empty() :
+				bbcode_text += error_text[error_at]
+				error_at += 1
+			elif type == CRITICAL && not critical_text.empty() :
+				bbcode_text += critical_text[critical_at]
+				critical_at += 1
+				
 	
 
