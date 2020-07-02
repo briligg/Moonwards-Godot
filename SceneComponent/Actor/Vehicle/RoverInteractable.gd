6896d78961621bde52a5e2117293cffb6d464ff3
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-07-02 23:42:56 +0200

    Add player ability to ride and exit rover

--- SceneComponent/Actor/Vehicle/RoverInteractable.gd
+++ SceneComponent/Actor/Vehicle/RoverInteractable.gd
@@ -1,5 +1,10 @@
 extends AComponent
 
+var interactee
+var interactee_cam
+
+var is_active = false
+
 func _init().("RoverInteractable", true):
 	pass
 # Called when the node enters the scene tree for the first time.
@@ -7,9 +12,26 @@ func _ready():
 	$Interactable.connect("interacted_by", self, "interacted_by")
 	$Interactable.display_info = "Control the rover"
 
-func interacted_by(interactee) -> void:
-	interactee.disable()
-	entity.enable()
+func interacted_by(e) -> void:
+	if !self.is_active:
+		self.interactee = e
+		self.interactee_cam = interactee.get_component("Camera").camera
+		self.interactee.disable()
+		self.entity.enable()
+		var cam = entity.get_component("Camera")
+		if cam:
+			cam.camera.current = true
+		is_active = true
 
 func disable():
 	pass
+
+func return_control() -> void:
+	self.interactee.enable()
+	self.interactee_cam.current = true
+	self.entity.disable()
+	self.is_active = false
+
+func _input(event : InputEvent) -> void :
+	if event.is_action_pressed("use") and is_active:
+		call_deferred("return_control")
