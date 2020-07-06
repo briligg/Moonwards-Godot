be94eff9a3c14e68b367f9e1db676411f9916ba8
Adrian Winkler <adrwinkler@gmail.com>
2020-07-06 17:19:52 +0200

    Slide left and right animations (#84)

--- SceneComponent/Display/MakersMonument/MonumentContent.gd
+++ SceneComponent/Display/MakersMonument/MonumentContent.gd
@@ -1,8 +1,10 @@
 extends Control
 
 var is_all: bool = false
+var idx: int = 0
 
-const MAX_ENTRIES: int = 6
+const MAX_ENTRIES: int = 5
+const ANIMATION_LENGTH: float = 18.0
 
 onready var sequence_area: Control = $VBoxContainer/SequenceArea
 onready var all_area: Control = $VBoxContainer/AllArea
@@ -41,7 +43,7 @@ var entries: Array = [
 	"sound": "jankowalski.wav"},
 	{"name": "Jan Kowalski",
 	"sound": "jankowalski.wav"},
-	{"name": "Jan Kowalki",
+	{"name": "Jan Kowalski",
 	"sound": "jankowalski.wav"},
 ]
 
@@ -58,32 +60,38 @@ func _ready():
 	
 	_build_all()
 	_build_sequence_buttons()
-	emit_signal("sequence_finished", 0)
+	_on_animation_finished("")
 
 
-func _on_sequence_finished(idx) -> void:
+func _on_animation_finished(_name: String) -> void:
 	for c in containers:
 		c.hide()
 	
 	containers[idx].show()
 	animation_players[idx].play("animation")
+	
+	idx += 1
+	if idx >= containers.size():
+		idx = 0
 
 
 func _build_sequence_buttons() -> void:
 	var i: int = 0
 	var j: int = -1
-	var c: VBoxContainer = null
+	var c: Control = null
 	var sequence_animation: AnimationPlayer = null
 	var animation: Animation = null
 	
 	for e in entries:
 		if i % MAX_ENTRIES == 0:
 			j += 1
-			c = VBoxContainer.new()
+			c = Control.new()
 			c.add_constant_override("separation", 64)
 			sequence_animation = AnimationPlayer.new()
+			sequence_animation.name = "AnimationPlayer"
+			sequence_animation.connect("animation_finished", self, "_on_animation_finished")
 			animation = Animation.new()
-			animation.length = 16.0
+			animation.length = ANIMATION_LENGTH
 			
 			sequence_animation.add_animation("animation", animation)
 			
@@ -93,26 +101,31 @@ func _build_sequence_buttons() -> void:
 			containers.append(c)
 			sequence_area.add_child(c)
 		
-		containers[j].add_child(_build_button(e, true, sequence_animation, animation))
+
+		containers[j].add_child(_build_button(e, true, animation, i, j))
 		i += 1
 
 
 func _build_button(var e: Dictionary, var is_animated: bool,
-		 var sequence_animation: AnimationPlayer, var animation: Animation) -> Button:
+		 var animation: Animation, var i: int, var j: int) -> Button:
 	
 	var button: Button = Button.new()
 	button.text = e["name"]
 	button.rect_min_size = Vector2(360, 80)
 	button.theme = monument_theme
 	button.set("custom_fonts/font", monument_font)
+	button.rect_position = Vector2(-600, -600)
 	
-	if is_animated and sequence_animation != null and animation != null:
+	if is_animated and animation != null:
+		button.name = "Button" + str(i)
 		var track_index = animation.add_track(Animation.TYPE_VALUE)
 		animation.track_set_path(track_index, button.name + ":rect_position")
-		animation.track_insert_key(track_index, 0.0, Vector2(0, 0))
-		animation.track_insert_key(track_index, 16.0, Vector2(2600, 0))
-		sequence_animation.add_animation("animation", animation)
-	
+		if i % 2 == 0:
+			animation.track_insert_key(track_index, 0.0, Vector2(-500, (i-j*MAX_ENTRIES)*150))
+			animation.track_insert_key(track_index, ANIMATION_LENGTH, Vector2(2100, (i-j*MAX_ENTRIES)*150))
+		else:
+			animation.track_insert_key(track_index, 0.0, Vector2(2100, (i-j*MAX_ENTRIES)*150))
+			animation.track_insert_key(track_index, ANIMATION_LENGTH, Vector2(-500, (i-j*MAX_ENTRIES)*150))
 	return button
 
 
@@ -124,7 +137,7 @@ func _build_all() -> void:
 	vbox.rect_min_size = Vector2(1920, 960)
 	
 	for e in entries:
-		var button: Button = _build_button(e, false, null, null)
+		var button: Button = _build_button(e, false, null, 0, 0)
 		vbox.add_child(button)
 	
 	scroll.add_child(vbox)
