27f22a8703db29d7547b9a27cad25de3c312ac77
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-07-11 06:48:35 +0200

    Refactor rover ride

--- SceneComponent/Actor/Vehicle/AthleteRover/RoverRideInteractable.gd
+++ SceneComponent/Actor/Vehicle/AthleteRover/RoverRideInteractable.gd
@@ -16,28 +16,24 @@ func _ready():
 	interactable.title = "Athlete Rover"
 
 func interacted_by(e) -> void:
-	if !self.is_active:
-		take_control(e)
+	if !self.is_active and e != self.entity:
+		call_deferred("take_control", e)
 	elif self.is_active:
-		return_control()
+		call_deferred("return_control")
 
 func take_control(e):
 	interactable.display_info = "Dismount the rover"
 	self.interactee = e
-	self.interactee_cam = interactee.get_component("Camera").camera
 	self.interactee.disable()
 	self.entity.enable()
-	var cam = entity.get_component("Camera")
-	if cam:
-		cam.camera.current = true
+
 	is_active = true
 		
 func return_control() -> void:
 	interactable.display_info = "Take control of the rover"
-	self.interactee.enable()
-	self.interactee_cam.current = true
 	self.entity.disable()
 	self.is_active = false
+	self.interactee.enable()
 
 func disable():
 	pass
