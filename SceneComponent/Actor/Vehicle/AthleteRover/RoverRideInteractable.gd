85e6cdf083cb5e5ca859831bfd8e4c3b01e4c05a
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-07-11 05:31:35 +0200

    Refactor rover ride interactable

--- SceneComponent/Actor/Vehicle/AthleteRover/RoverRideInteractable.gd
+++ SceneComponent/Actor/Vehicle/AthleteRover/RoverRideInteractable.gd
@@ -5,33 +5,39 @@ var interactee_cam
 
 var is_active = false
 
+onready var interactable = $Interactable
+
 func _init().("RoverRideInteractable", true):
 	pass
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Interactable.connect("interacted_by", self, "interacted_by")
-	$Interactable.display_info = "Control the rover"
+	interactable.connect("interacted_by", self, "interacted_by")
+	interactable.display_info = "Take control of the rover"
+	interactable.title = "Athlete Rover"
 
 func interacted_by(e) -> void:
 	if !self.is_active:
-		self.interactee = e
-		self.interactee_cam = interactee.get_component("Camera").camera
-		self.interactee.disable()
-		self.entity.enable()
-		var cam = entity.get_component("Camera")
-		if cam:
-			cam.camera.current = true
-		is_active = true
+		take_control(e)
+	elif self.is_active:
+		return_control()
 
-func disable():
-	pass
-
+func take_control(e):
+	interactable.display_info = "Dismount the rover"
+	self.interactee = e
+	self.interactee_cam = interactee.get_component("Camera").camera
+	self.interactee.disable()
+	self.entity.enable()
+	var cam = entity.get_component("Camera")
+	if cam:
+		cam.camera.current = true
+	is_active = true
+		
 func return_control() -> void:
+	interactable.display_info = "Take control of the rover"
 	self.interactee.enable()
 	self.interactee_cam.current = true
 	self.entity.disable()
 	self.is_active = false
 
-func _input(event : InputEvent) -> void :
-	if event.is_action_pressed("use") and is_active:
-		call_deferred("return_control")
+func disable():
+	pass
