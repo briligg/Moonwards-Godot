87b79bbaf6e6b67f43478f09ff631a4ef34da725
Zach777 <MeteorCity3109@protonmail.com>
2020-08-10 09:16:10 -0400

    Add camera flight mode

--- SceneComponent/Actor/CameraController.gd
+++ SceneComponent/Actor/CameraController.gd
@@ -15,6 +15,9 @@ var mouse_sensitivity: float = 0.1
 var yaw: float = 0.0
 var pitch: float = 0.0
 
+#Determines if I should freely fly or not.
+var is_flying : bool = false
+
 func _init().("Camera", true):
 	pass
 	
@@ -25,21 +28,55 @@ func _ready() -> void:
 	camera.set_as_toplevel(true)
 
 func _process(_delta: float) -> void:
+	#Set the camera back to regular mode if it is not the current camera.
+	if not camera.is_current() :
+		is_flying = false
+	
 	var _new_rot = Vector3(deg2rad(pitch), deg2rad(yaw), 0.0)
 	camera.set_rotation(_new_rot)
 	_update_cam_pos()
-	var t = camera.global_transform.origin
-	entity.look_dir.y = entity.global_transform.origin.y
-	entity.look_dir.x = t.x
-	entity.look_dir.z = t.z
+	
+	if not is_flying :
+		var t = camera.global_transform.origin
+		entity.look_dir.y = entity.global_transform.origin.y
+		entity.look_dir.x = t.x
+		entity.look_dir.z = t.z
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		var mouse_vec : Vector2 = event.get_relative()
 		yaw = fmod(yaw - mouse_vec.x * mouse_sensitivity, 360.0)
 		pitch = max(min(pitch - mouse_vec.y * mouse_sensitivity , max_pitch), -max_pitch)
+	
+	#Check to see if the player has started camera free fly.
+	elif event.is_action_pressed("toggle_camera_fly") :
+		is_flying = !is_flying
+		Signals.Entities.emit_signal(Signals.Entities.FREE_CAMERA_TOGGLED)
 
-func _update_cam_pos() -> void:
+func _update_cam_pos(delta : float = 0.016667) -> void:
+	#The player is in camera fly mode.
+	if is_flying :
+		var input : Vector3 = Vector3.ZERO
+		input.z = int(Input.is_action_pressed("move_forwards")) + -int(Input.is_action_pressed("move_backwards"))
+		input.x = int(Input.is_action_pressed("move_left")) + -int(Input.is_action_pressed("move_right"))
+		
+		#Get input relative to the camera's looking direction.
+		var new_input : Vector3 = input
+		var forward = camera.global_transform.basis.z
+		var left = camera.global_transform.basis.x
+		new_input = -(input.z * forward + input.x * left)
+		
+		input.y = int(Input.is_action_pressed("fly_up")) + -int(Input.is_action_pressed("fly_down"))
+		new_input.y = input.y
+		
+		var velocity : Vector3 = new_input
+		velocity *= 90
+		velocity *= delta
+		
+		camera.global_transform.origin += velocity
+	
+		return
+	
 	var new_cam_pos = global_transform.origin - _get_cam_normal() * dist
 	# Check if the new pos is behind collidable objects
 	var ray = get_world().direct_space_state.intersect_ray(pivot.global_transform.origin, new_cam_pos, excluded_cull_bodies)
--- Script/Manager/SignalsManager/EntitySignals.gd
+++ Script/Manager/SignalsManager/EntitySignals.gd
@@ -7,8 +7,10 @@ const name = "Entities"
 # Define the signal's string name.
 const ENTITY_CREATED: String = "entity_created"
 const FLY_TOGGLED : String = "fly_toggled"
+const FREE_CAMERA_TOGGLED : String = "free_camera_toggled"
 
 
 # Define the actual signal.
 signal entity_created(entity)
 signal fly_toggled()
+signal free_camera_toggled()
