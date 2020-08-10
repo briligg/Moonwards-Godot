ef48ddec3e957e5025dfa37cf2beb06a595dd86e
Zach777 <MeteorCity3109@protonmail.com>
2020-08-09 21:21:59 -0400

    Add fly state to HumanPlayer

--- SceneComponent/Actor/Humanoid/HumanoidInput.gd
+++ SceneComponent/Actor/Humanoid/HumanoidInput.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 	
 func _process_client(_delta: float) -> void:
 	entity.input = Vector3.ZERO
+	entity.fly_input_float = 0
 	handle_input()
 
 func handle_input() -> void:
@@ -30,6 +31,16 @@ func handle_input() -> void:
 		entity.input.x += 1
 	elif Input.is_action_pressed("move_right"):
 		entity.input.x += -1
+	
+	#Start flying if the player requested it.
+	if Input.is_action_just_pressed("toggle_fly"):
+		Signals.Entities.emit_signal(Signals.Entities.FLY_TOGGLED)
+	
+	#Fly upwards if the player requested it.
+	if Input.is_action_pressed("fly_up") :
+		entity.fly_input_float += 1
+	elif Input.is_action_pressed("fly_down") :
+		entity.fly_input_float -= 1
 
 func set_ignore_inputs(ignore_bool : bool) -> void :
 	ignore_inputs = ignore_bool
--- Script/Manager/SignalsManager/EntitySignals.gd
+++ Script/Manager/SignalsManager/EntitySignals.gd
@@ -6,6 +6,9 @@ const name = "Entities"
 
 # Define the signal's string name.
 const ENTITY_CREATED: String = "entity_created"
+const FLY_TOGGLED : String = "fly_toggled"
+
 
 # Define the actual signal.
 signal entity_created(entity)
+signal fly_toggled()
