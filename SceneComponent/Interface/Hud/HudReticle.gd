dbab028598aa09903cbe91c2c41ca91353d12ffe
Zach777 <MeteorCity3109@protonmail.com>
2020-08-17 07:47:46 -0400

    Active camera gets HudReticle RayCast

--- SceneComponent/Interface/Hud/HudReticle.gd
+++ SceneComponent/Interface/Hud/HudReticle.gd
@@ -1,6 +1,21 @@
 extends TextureRect
 
 
+var camera : Camera
+
+onready var ray_cast : RayCast = get_node("ReticleRayCast")
+
+
+func _physics_process(_delta) -> void :
+	var current_camera : Camera = get_tree().root.get_camera()
+	if current_camera != camera :
+		camera = current_camera
+		ray_cast.get_parent().remove_child(ray_cast)
+		current_camera.add_child(ray_cast)
+
+#Listen to SignalsManager to see when I should activate/deactivate.
+func _ready() -> void :
+	Signals.Hud.connect(Signals.Hud.RETICLE_ACTIVITY_SET, self, "_set_activity")
 
 #Determines if I should turn on when requested or not.
 func _set_activity(is_active : bool) -> void :
@@ -11,7 +26,3 @@ func _set_activity(is_active : bool) -> void :
 		hide()
 		Signals.Hud.disconnect(Signals.Hud.SHOW_RETICLE, self, "show")
 		Signals.Hud.disconnect(Signals.Hud.HIDE_RETICLE, self, "hide")
-
-#Listen to SignalsManager to see when I should activate/deactivate.
-func _ready() -> void :
-	Signals.Hud.connect(Signals.Hud.RETICLE_ACTIVITY_SET, self, "_set_activity")
