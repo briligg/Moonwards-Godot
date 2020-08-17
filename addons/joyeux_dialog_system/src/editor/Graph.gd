8c26e4857c35617d1c0ac916d6853d1c29337015
yael <yael.atletl@gmail.com>
2020-08-17 11:27:13 -0500

    Added editor on lower panel

---
+++ addons/joyeux_dialog_system/src/editor/Graph.gd
@@ -0,0 +1,18 @@
+tool
+extends GraphEdit
+
+
+func _on_GraphEdit_popup_request(position):
+	pass # Replace with function body.
+
+
+func _on_GraphEdit_delete_nodes_request():
+	pass # Replace with function body.
+
+
+func _on_GraphEdit_connection_request(from, from_slot, to, to_slot):
+	pass # Replace with function body.
+
+
+func _on_GraphEdit_disconnection_request(from, from_slot, to, to_slot):
+	pass # Replace with function body.
