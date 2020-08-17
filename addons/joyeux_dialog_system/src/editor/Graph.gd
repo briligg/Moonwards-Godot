662d55e12885d99db27ce6b424a3dcb6f7de8a45
yael <yael.atletl@gmail.com>
2020-08-17 15:07:40 -0500

    Can add dialog nodes, nodes can be edited

--- addons/joyeux_dialog_system/src/editor/Graph.gd
+++ addons/joyeux_dialog_system/src/editor/Graph.gd
@@ -3,7 +3,7 @@ extends GraphEdit
 
 
 func _on_GraphEdit_popup_request(position):
-	pass # Replace with function body.
+	create_node()
 
 
 func _on_GraphEdit_delete_nodes_request():
@@ -16,3 +16,15 @@ func _on_GraphEdit_connection_request(from, from_slot, to, to_slot):
 
 func _on_GraphEdit_disconnection_request(from, from_slot, to, to_slot):
 	pass # Replace with function body.
+
+func create_node(text: String = "", input : String = "", override : String = "", offset : Vector2 = Vector2.ZERO):
+	var node = GraphNode.new()
+	var text_popup = TextPopup.new(text, input, override)
+	node.offset = offset
+	node.title = text
+	var popbutton = Button.new()
+	popbutton.text = "Edit"
+	popbutton.connect("pressed", text_popup, "popup")
+	node.add_child(popbutton)
+	node.add_child(text_popup)
+	add_child(node)
