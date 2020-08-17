28074d2aca89db8515ab61eb605faa56112787c4
Zach777 <MeteorCity3109@protonmail.com>
2020-08-17 14:25:57 -0400

    No error when setting reticle the same way twice

--- SceneComponent/Interface/Hud/HudReticle.gd
+++ SceneComponent/Interface/Hud/HudReticle.gd
@@ -12,6 +12,10 @@ func _physics_process(_delta) -> void :
 		camera = current_camera
 		ray_cast.get_parent().remove_child(ray_cast)
 		current_camera.add_child(ray_cast)
+		
+		#Make sure Ray Cast has 0,0,0 transformation so it is directly
+		#on it's camera parent.
+		ray_cast.transform.origin = Vector3.ZERO
 
 #Listen to SignalsManager to see when I should activate/deactivate.
 func _ready() -> void :
@@ -20,9 +24,13 @@ func _ready() -> void :
 #Determines if I should turn on when requested or not.
 func _set_activity(is_active : bool) -> void :
 	if is_active :
-		Signals.Hud.connect(Signals.Hud.SHOW_RETICLE, self, "show")
-		Signals.Hud.connect(Signals.Hud.HIDE_RETICLE, self, "hide")
+		if not Signals.Hud.is_connected(Signals.Hud.SHOW_RETICLE, self, "show") :
+			Signals.Hud.connect(Signals.Hud.SHOW_RETICLE, self, "show")
+			Signals.Hud.connect(Signals.Hud.HIDE_RETICLE, self, "hide")
+		ray_cast.enabled = true
 	else :
 		hide()
-		Signals.Hud.disconnect(Signals.Hud.SHOW_RETICLE, self, "show")
-		Signals.Hud.disconnect(Signals.Hud.HIDE_RETICLE, self, "hide")
+		if Signals.Hud.is_connected(Signals.Hud.SHOW_RETICLE, self, "show") :
+			Signals.Hud.disconnect(Signals.Hud.SHOW_RETICLE, self, "show")
+			Signals.Hud.disconnect(Signals.Hud.HIDE_RETICLE, self, "hide")
+		ray_cast.enabled = false
