46b4898773d26e2c80d483bcf8fd8f3f65602280
Zach777 <MeteorCity3109@protonmail.com>
2020-08-18 15:27:35 -0400

    Create more in depth InputEventMouseButton

--- SceneComponent/Interface/Hud/ReticleRayCast.gd
+++ SceneComponent/Interface/Hud/ReticleRayCast.gd
@@ -3,7 +3,7 @@ extends RayCast
 
 onready var viewport : Viewport = get_tree().root
 
-func _physics_process(_delta) -> void :
+func _input(event) -> void :
 	#Look for collisions so I can convert them to clicks.
 	force_raycast_update()
 	if is_colliding() :
@@ -12,15 +12,20 @@ func _physics_process(_delta) -> void :
 			#Create a new event for the object to read. 
 			#I am emulating a mouse cursor.
 			var new_event : InputEventMouse 
-			if Input.is_action_just_pressed("left_click") :
+			if event.is_action_pressed("left_click") :
 				#Set the InputEvent to left mouse button.
 				new_event = InputEventMouseButton.new()
 				new_event.button_index = BUTTON_LEFT
 				new_event.button_mask = BUTTON_MASK_LEFT
 				new_event.pressed = true
+				new_event.command = false
+				new_event.device = 0
+				new_event.factor = 1
+				new_event.doubleclick = false
 			else :
 				new_event = InputEventMouseMotion.new()
-			get_collider().emit_signal("input_event", null, new_event, get_collision_point(), get_collision_normal(), null)
+			var camera : Camera = get_tree().root.get_camera()
+			get_collider().emit_signal("input_event", camera, new_event, get_collision_point(), get_collision_normal(), 0)
 
 func _ready() :
 	set_physics_process(false)
