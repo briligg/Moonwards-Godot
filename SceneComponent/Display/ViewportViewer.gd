74769b28b6296eb4e47fcea37a5e32206c748e9c
Zach777 <MeteorCity3109@protonmail.com>
2020-08-31 18:20:26 -0400

    Feat/fix makers monument#515 (#524)
    * Better button text on MomumentContent
    
    * ViewportViewer clicks the correct location now
    
    * ViewportViewer can have NodePath or PackedScenes

--- SceneComponent/Display/ViewportViewer.gd
+++ SceneComponent/Display/ViewportViewer.gd
@@ -19,13 +19,23 @@ onready var viewport: Node = get_node("Viewport")
 
 var content_instance = null
 export(PackedScene) var content = null
+export(NodePath) var content_as_child = null
 export(bool) var hologram = false
 
 
 func _ready():
 	set_process_input(false)
 	if content != null:
+		#Do not allow both Content and content as child to
+		#be set together.
+		assert(content_as_child == null)
 		content_instance = content.instance()
+		viewport.add_child(content_instance)
+	
+	elif content_as_child != null :
+		#The NodePath given must be a child of myself.
+		content_instance = get_node(content_as_child)
+		remove_child(content_instance)
 		viewport.add_child(content_instance)
 	else:
 		Log.trace(self, "_ready", "Screen View without a content")
@@ -34,17 +44,17 @@ func _ready():
 	
 	
 	if hologram:
-		var mat = $Area/Quad.get_surface_material(0)
+		var mat = $Area/CollisionShape/Quad.get_surface_material(0)
 		mat.albedo_color.a = 0.7
 		mat.flags_transparent = true
-		$Area/Quad.set_surface_material(0, mat)
+		$Area/CollisionShape/Quad.set_surface_material(0, mat)
 
 # Mouse events for Area
 func _on_area_input_event(_camera, event, click_pos, _click_normal, _shape_idx):
-	
 	if !(event is InputEventMouseButton):
 		return
 		
+	#Get the size of the viewport and it's world position.
 	var screen_pos = screen.global_transform.origin
 	var screen_size = collision_box.scale
 	
