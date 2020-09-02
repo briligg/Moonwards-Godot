e586a2a2eb1296ad06915fc06a50ea643134f870
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-09-02 19:59:50 +0200

    Refactor billboard & cleanup
    - Renamed SlidePresentation to billboard
    - Removed dependency on events in ViewportViewer, it is not just a base class that provides input to a 2d viewport in the 3d world

---
+++ SceneComponent/Display/Billboard/Billboard.gd
@@ -0,0 +1,51 @@
+"""
+
+	Use this class for showing-up independent screens within the game.
+
+"""
+
+tool
+extends Spatial
+
+# Member variables
+onready var screen = $Screen
+onready var slide_control = $Screen.content_instance
+
+export(Array, Texture) var texture_slides
+export(Array, String) var text_slides
+
+var slide_2d_control
+var slide_index: int = 0
+var slide_size: int = 0
+
+func _ready():
+	slide_control.connect("next_pressed", self, "_on_next_pressed")
+	slide_control.connect("prev_pressed", self, "_on_prev_pressed")
+	
+	if texture_slides.size() != text_slides.size():
+		assert(false)
+		Log.error(self, "_ready", "Texture array size is not the same as text array size.")
+	else:
+		slide_size = texture_slides.size()
+	
+	#At start of scene, make SlidePresentation display first slide.
+	if not text_slides.empty() :
+		slide_control.set_text(text_slides[0])
+		slide_control.set_texture(texture_slides[0])
+	
+func _on_next_pressed():
+	if slide_size > slide_index + 1:
+		slide_index += 1
+	else:
+		slide_index = 0
+	slide_control.texture = texture_slides[slide_index]
+	slide_control.text = text_slides[slide_index]
+
+func _on_prev_pressed():
+	if slide_index > 0:
+		slide_index -= 1
+	else:
+		slide_index = slide_size - 1
+	slide_control.texture = texture_slides[slide_index]
+	slide_control.text = text_slides[slide_index]
+
--- SceneComponent/Display/SlidePresentation/SlideResource.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-
-export(Texture) var texture: Texture
-export(String) var text: String
