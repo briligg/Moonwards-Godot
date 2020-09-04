f890a2ca755aa8a1cfe5648b37f394bef6ae21cf
Zach777 <MeteorCity3109@protonmail.com>
2020-09-03 21:03:02 -0400

    Test responds to mouse click pressed not released

--- SceneComponent/Display/Tooltip/test.gd
+++ SceneComponent/Display/Tooltip/test.gd
@@ -11,10 +11,6 @@ func _ready() -> void:
 	get_node("Area").connect("input_event", self, "on_input_event")
 
 func on_input_event(_camera, event, click_pos, _click_normal, _shape_idx):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton && event.pressed :
 		Log.warning(self, "" , "Tooltip question mark received a click")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+		
