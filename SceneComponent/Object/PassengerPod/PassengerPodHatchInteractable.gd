dbd6b9a1ae696cc164427705168af1abe08b6c88
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-09-04 20:06:50 +0200

    WIP

--- SceneComponent/Object/PassengerPod/PassengerPodHatchInteractable.gd
+++ SceneComponent/Object/PassengerPod/PassengerPodHatchInteractable.gd
@@ -2,6 +2,8 @@ extends Spatial
 export(NodePath) var hatch_collision_path
 onready var hatch_collision = get_node(hatch_collision_path)
 
+onready var anim = get_parent().get_node("Model/LOD0/HatchAnimation")
+
 var is_hatch_open: bool = false
 
 func _ready() -> void:
@@ -16,11 +18,13 @@ func interacted_by(interactor):
 
 
 func close_hatch():
+	anim.play("Close" ,1, 1, false)
 	is_hatch_open = false
 	hatch_collision.disabled = false
 	$Interactable.display_info = "Open Hatch"
 
 func open_hatch():
+	anim.play("Open")
 	is_hatch_open = true
 	hatch_collision.disabled = true
 	$Interactable.display_info = "Close Hatch"
