3be3b3fcb0c35f0165675b13d78b723cf825d50b
Zach777 <MeteorCity3109@protonmail.com>
2020-09-08 20:04:17 -0400

    Add pressing animation to TooltipButton

--- SceneComponent/Display/Tooltip/TooltipButton.gd
+++ SceneComponent/Display/Tooltip/TooltipButton.gd
@@ -3,6 +3,14 @@ extends Spatial
 #Buttons for the TooltipData to use.
 var tooltip_data : TooltipData = TooltipData.new()
 
+#When hovered over, this is true. When not hovered, this is false.
+#var mouse_hovered : bool = false
+
+#Allows pressed animation.
+var pressed : bool = false
+
+onready var anim_player : AnimationPlayer = $Anim
+onready var mesh : MeshInstance = $Area/Spatial
 
 export var bbtext_fields : PoolStringArray = []
 
@@ -19,7 +27,19 @@ func _ready() -> void:
 	tooltip_data.bbtext_fields = bbtext_fields
 	tooltip_data.title = title
 
+#Set mesh instance back to it's default values.
+func _reset_mesh() -> void :
+	mesh.scale = Vector3(1,1,1)
+
 func on_input_event(_camera, event, _click_pos, _click_normal, _shape_idx):
-	if event is InputEventMouseButton && event.pressed :
-		Log.warning(self, "" , "Tooltip question mark received a click")
-		Signals.Hud.emit_signal(Signals.Hud.TOOLTIP_MENU_DISPLAYED, tooltip_data)
+	if event is InputEventMouseButton :
+		if event.pressed :
+			anim_player.play("pressed")
+		
+		#Only bring up TooltipDisplay if the player has pressed and released me.
+		if event.pressed == false :
+			anim_player.stop()
+			_reset_mesh()
+			pressed = false
+			Log.warning(self, "" , "Tooltip question mark received a click")
+			Signals.Hud.emit_signal(Signals.Hud.TOOLTIP_MENU_DISPLAYED, tooltip_data)
