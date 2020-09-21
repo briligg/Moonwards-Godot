ddd20e1bb45943a038ae530377161cfe8eca9c5e
Zach777 <MeteorCity3109@protonmail.com>
2020-09-20 22:06:47 -0400

    Map can be shown and hidden

---
+++ Tree/Interface/Hud/AppsContainer.gd
@@ -0,0 +1,29 @@
+extends Container
+
+
+#Provide simple undo and switching functionality.
+var current_app : Node = null
+var previous_app : Node = null
+
+
+#Look for the visible app and set it as current.
+func _ready() -> void :
+	for app in get_children() :
+		if app.visible :
+			current_app = app
+			previous_app = app
+			break
+
+#A new app node has become active. Hide the previous app.
+func new_active_app(app_node : Node) -> void :
+	current_app.hide()
+	if app_node != current_app :
+		previous_app = current_app
+	current_app = app_node
+	current_app.show()
+
+#Go back to the previous app.
+func revert_active_app() -> void :
+	current_app.visible = false
+	current_app = previous_app
+	previous_app.visible = true
--- Tree/Interface/Hud/Map/HudMap.gd
+++
@@ -1,8 +0,0 @@
-extends Panel
-
-
-func _ready() -> void :
-	Signals.Hud.connect(Signals.Hud.MAP_VISIBILITY_SET, self, "_set_visibility")
-
-func _set_visibility(become_visible : bool) -> void :
-	visible = become_visible
