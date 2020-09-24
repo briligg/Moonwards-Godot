599da163abf045240a0a93b51a4860ac9cd4ac9a
Zach777 <MeteorCity3109@protonmail.com>
2020-09-24 12:16:02 -0400

    Billboard presents slides correctly

--- SceneComponent/Display/Billboard/Billboard.gd
+++ SceneComponent/Display/Billboard/Billboard.gd
@@ -14,7 +14,7 @@ onready var slide_control = $Screen.content_instance
 export(Array, Texture) var texture_slides
 export(Array, String) var text_slides
 
-var slide_2d_control
+
 var slide_index: int = 0
 var slide_size: int = 0
 
@@ -23,8 +23,8 @@ func _ready():
 	slide_control.connect("prev_pressed", self, "_on_prev_pressed")
 	
 	if texture_slides.size() != text_slides.size():
-		assert(false)
 		Log.error(self, "_ready", "Texture array size is not the same as text array size.")
+		assert(false)
 	else:
 		slide_size = texture_slides.size()
 	
@@ -34,8 +34,11 @@ func _ready():
 		slide_control.set_texture(texture_slides[0])
 	
 func _on_next_pressed():
+	#If we have more slides to go to, go to the next one.
 	if slide_size > slide_index + 1:
 		slide_index += 1
+	
+	#We have reached the end of the slides. Cycle back to the beginning.
 	else:
 		slide_index = 0
 	slide_control.texture = texture_slides[slide_index]
