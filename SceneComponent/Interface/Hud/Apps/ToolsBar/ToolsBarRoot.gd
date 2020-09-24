13da38cbfac7b526008b5322e8e2ba2b7bc588f7
Zach777 <MeteorCity3109@protonmail.com>
2020-09-23 21:08:01 -0400

    Blur background when NPC editor is visible

--- SceneComponent/Interface/Hud/Apps/ToolsBar/ToolsBarRoot.gd
+++ SceneComponent/Interface/Hud/Apps/ToolsBar/ToolsBarRoot.gd
@@ -3,7 +3,7 @@ extends PanelContainer
 
 func _ready() -> void :
 	var button = $VBoxContainer/HBoxContainer_Dynamic/VBoxContainer5/MarginContainer/Button_NPC
-	button.connect("pressed", self, "pressed", ["NPCApp"])
+	button.connect("pressed", self, "pressed", ["NPCApp", true])
 
-func pressed(app_name : String) -> void :
-	get_parent().change_app(app_name)
+func pressed(app_name : String, blur_background : bool) -> void :
+	get_parent().change_app(app_name, blur_background)
--- Tree/Interface/Hud/AppsContainer.gd
+++ Tree/Interface/Hud/AppsContainer.gd
@@ -5,6 +5,10 @@ extends Container
 var current_app : Node = null
 var previous_app : Node = null
 
+#Blurred background reference for quick access.
+onready var blur : TextureRect = get_node("../Blur")
+#Lets us blur the background when reverting if needed.
+var previous_app_blurred : bool = false
 
 #Look for the visible app and set it as current.
 func _ready() -> void :
@@ -15,20 +19,26 @@ func _ready() -> void :
 			break
 
 #Change the active app by passing it's node name.
-func change_app(app_name : String) -> void :
+func change_app(app_name : String, blur_background : bool = false) -> void :
 	assert(has_node(app_name))
-	new_active_app(get_node(app_name))
+	new_active_app(get_node(app_name), blur_background)
 
 #A new app node has become active. Hide the previous app.
-func new_active_app(app_node : Node) -> void :
+func new_active_app(app_node : Node, blurred_background : bool = false) -> void :
 	current_app.hide()
 	if app_node != current_app :
 		previous_app = current_app
 	current_app = app_node
 	current_app.show()
+	
+	#Show the blurred background if specified.
+	previous_app_blurred = blur.visible
+	blur.visible = blurred_background
 
 #Go back to the previous app.
 func revert_active_app() -> void :
 	current_app.visible = false
 	current_app = previous_app
 	previous_app.visible = true
+	
+	blur.visible = previous_app_blurred
