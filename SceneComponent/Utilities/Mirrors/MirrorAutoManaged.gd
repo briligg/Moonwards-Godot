3e1f6188e379fd1a8265f88bb172e7f5e01bd45c
Hevedy <Hevedy@users.noreply.github.com>
2020-09-28 22:34:55 +0200

    Try to get the camera and mirror pos in order to get the mirro effect
    No luck right now at get the camera etc properly

--- SceneComponent/Utilities/Mirrors/MirrorAutoManaged.gd
+++ SceneComponent/Utilities/Mirrors/MirrorAutoManaged.gd
@@ -41,6 +41,31 @@ func printTexture():
 	else:
 		return
 
+func getMirrorEffect():
+	var mNormal = localPlane.get_global_transform().basis.z.normalized()
+	var mOrigin = localPlane.get_global_transform().origin
+	var mTransform = localPlane.get_global_transform()
+	var mPlane = Plane(mNormal, mOrigin.dot(mNormal))
+	
+	var playerCamPos
+	if is_instance_valid(playerCamera):
+		playerCamPos = playerCamera.get_global_transform().origin
+	else:
+		playerCamPos = Vector3.ZERO
+	
+	var mOffset = -mTransform.xform_inv(playerCamPos)
+	mOffset = Vector2(mOffset.x,mOffset.y)
+	var mProjection = mPlane.project(playerCamPos)
+	var mPos = playerCamPos + (mProjection - playerCamPos) * CamHeight
+	
+	if is_instance_valid(localCamera) and is_instance_valid(localPlane):
+		#localCamera.set_global_transform(t)
+		#localCamera.set_frustum()
+		localCamera.set_frustum(8.0, mOffset, mProjection.distance_to(playerCamPos), 500.0) #localPlane.scale.get get the scale rather automatically
+	else:
+		return
+
+
 func getMainData():
 	playerCamera = get_tree().get_root().get_viewport().get_camera() 
 	#var playerLoc
@@ -66,15 +91,10 @@ func _ready() -> void:
 		return
 
 
-func _process(delta : float) -> void:
+func _process(_delta : float) -> void:
 	if not Engine.is_editor_hint():
-		var playerLoc
-		if is_instance_valid(playerCamera):
-			playerLoc = playerCamera.get_global_transform().origin
-			printTexture()
-		else:
-			playerLoc = Vector3.ZERO
-			return
+		getMirrorEffect()
+		printTexture()
 
 func _enter_tree():
 	pass
