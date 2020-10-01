31f4339cfca7306371f778a5ffb19d9c2e7f57dd
Karim Rizk <karim@rizk.dev>
2020-10-01 22:49:03 +0200

    Refactor player movement to use rigidbody (#615)
    * Initial player rigid movement
    
    * Update rigid movement & player rotation
    
    * Fix player freezing on moving the game window
    
    * Update movement & add jump
    
    * Fix double jump
    
    * Add realistic jumping
    
    * Hook up animations
    
    * Modify jump speed
    
    * Add jump debug
    
    * Fix jump equation
    
    * Also modified jump velocity
    
    * Revert to capsule collider
    
    * Update speeds
    
    * Fix animation
    
    * Update entity.velocity every tick
    
    * Add new flight mode to human player
    
    * Add climb stairs
    
    * Human positions itself outside the stairs pole
    
    * Remove useless code from HumanoidMovement.gd
    
    * Clean up the code in HumanoidMovement
    - Getting off VerticalStairs at the top still needs fixed.
    
    * Fix climbing stairs & cleanup
    
    * Add new pod collision asset
    
    * Fix movement on slopes WIP
    
    * Fix movement on slopes
    
    * Cleanup
    
    * Fix wrong slope calculation
    
    * More slope fixes
    
    * Finetuning
    
    Co-authored-by: Zach777 <MeteorCity3109@protonmail.com>

--- SceneComponent/Actor/Humanoid/HumanoidInput.gd
+++ SceneComponent/Actor/Humanoid/HumanoidInput.gd
@@ -13,7 +13,6 @@ func _ready() -> void:
 	
 func _process_client(_delta: float) -> void:
 	entity.input = Vector3.ZERO
-	entity.fly_input_float = 0
 	handle_input()
 
 func handle_input() -> void:
@@ -32,17 +31,6 @@ func handle_input() -> void:
 		entity.input.x += 1
 	elif Input.is_action_pressed("move_right"):
 		entity.input.x += -1
-	
-	#Fly upwards if the player requested it.
-	if ProjectSettings.get("debug/settings/gameplay/humans_can_fly") :
-		#Start flying if the player requested it.
-		if Input.is_action_just_pressed("toggle_fly"):
-			Signals.Entities.emit_signal(Signals.Entities.FLY_TOGGLED)
-		
-		if Input.is_action_pressed("fly_up") :
-			entity.fly_input_float += 1
-		elif Input.is_action_pressed("fly_down") :
-			entity.fly_input_float -= 1
 
 func set_ignore_inputs(ignore_bool : bool) -> void :
 	ignore_inputs = ignore_bool
