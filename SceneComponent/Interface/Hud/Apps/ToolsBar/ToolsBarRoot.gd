67fac1ca6c667dbb7911e13116355679af67f913
Zach777 <MeteorCity3109@protonmail.com>
2020-10-01 21:51:22 -0400

    Show the Building App when clicked

--- SceneComponent/Interface/Hud/Apps/ToolsBar/ToolsBarRoot.gd
+++ SceneComponent/Interface/Hud/Apps/ToolsBar/ToolsBarRoot.gd
@@ -4,6 +4,9 @@ extends PanelContainer
 func _ready() -> void :
 	var button = $VBoxContainer/HBoxContainer_Dynamic/VBoxContainer5/MarginContainer/Button_NPC
 	button.connect("pressed", self, "pressed", ["NPCApp", true])
+	
+	button = $VBoxContainer/HBoxContainer_Dynamic/VBoxContainer2/MarginContainer/Button_Build
+	button.connect("pressed", self, "pressed", ["BuildingApp", false])
 
 func pressed(app_name : String, blur_background : bool) -> void :
 	get_parent().change_app(app_name, blur_background)
