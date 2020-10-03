d8774da41b08cc082c8c924ef1d2e3f70f3536f2
Zach777 <MeteorCity3109@protonmail.com>
2020-10-02 22:51:56 -0400

    Submenus in RadialSelector reachable

--- SceneComponent/Interface/Hud/Radial/RadialSelector.gd
+++ SceneComponent/Interface/Hud/Radial/RadialSelector.gd
@@ -3,19 +3,30 @@ extends Container
 export var button_radius = 100 #in godot position units
 export var radial_width = 50 #in godot position units
 
+onready var actions : VBoxContainer = $ContainerActions
+onready var expressions : VBoxContainer = $ContainerExpression
+onready var main : HBoxContainer = $ContainerMain
+
 
 func _input(event : InputEvent) -> void :
 	if event.is_action_pressed("radial_menu") :
-		visible = !visible
+		toggle_visibility()
 
 # Place buttons in the correct location.
 func _ready():
-	var actions : Button
-	actions = $ContainerMain/MarginContainer/MarginContainer/Button_Actions
-	actions.connect("pressed", self, "hide")
+	var button : Button
+	button = $ContainerMain/MarginContainer/MarginContainer/Button_Actions
+	button.connect("pressed", self, "_show_menu", [actions])
+	
+	button = $ContainerMain/MarginContainer2/MarginContainer/Button_Expressions
+	button.connect("pressed", self, "_show_menu", [expressions])
 	
 	hide()
 	place_buttons()
+
+func _show_menu(submenu : Control) -> void :
+	submenu.show()
+	main.hide()
 
 #Repositions the buttons
 func place_buttons():
@@ -31,8 +42,8 @@ func place_buttons():
 	var angle = 0 #in radians
 	for btn in buttons: 
 		#calculate the x and y positions for the button at that angle
-		var x = cos(angle)*button_radius
-		var y = sin(angle)*button_radius
+		var _x = cos(angle)*button_radius
+		var _y = sin(angle)*button_radius
 
 		var circle_pos = Vector2(button_radius, 0).rotated(angle)
 		#set button's position
@@ -49,6 +60,14 @@ func place_buttons():
 
 		#Advance to next angle position
 		angle += angle_offset
+
+#Toggle radial menu visibility. It sets the menu back to being the inital setup.
+func toggle_visibility() -> void :
+	visible = !visible
+	
+	main.show()
+	expressions.hide()
+	actions.hide()
 
 #utility function for adding buttons and recalculating their positions
 #TODO: Should probably just use a signal to run place_button on any tree change
