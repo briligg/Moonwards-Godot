c1ba1e1048562e48c7e4442cc77d257c7d033ff2
Zach777 <MeteorCity3109@protonmail.com>
2020-10-04 22:20:58 -0400

    Higlight in an intuitive way when picking options

--- SceneComponent/Interface/Hud/Radial/RadialSelector.gd
+++ SceneComponent/Interface/Hud/Radial/RadialSelector.gd
@@ -27,19 +27,6 @@ func _ready():
 	
 	hide()
 	place_buttons()
-	
-	#Tenporary solution. Go through each button in the submenus and hide the menu
-	#if they are pressed.
-	_recurse_children(actions)
-	_recurse_children(expressions)
-
-#Temporary function for hiding the RadialSelector when a sub menu is pressed..
-func _recurse_children(node : Node) -> void :
-	if node is Button :
-		node.connect("pressed", self, "toggle_visibility")
-	
-	for child in node.get_children() :
-		_recurse_children(child)
 
 #This brings up the submenu that is related to the pressed button.
 func _show_menu(submenu : Control) -> void :
@@ -90,6 +77,8 @@ func toggle_visibility() -> void :
 	
 	else :
 		Hud.show(Hud.flags.AppMenusAll)
+		expressions.reset()
+		actions.reset()
 	
 	main.show()
 	expressions.hide()
