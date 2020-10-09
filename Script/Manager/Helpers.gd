cfde408637d3ae0f700a1a4b81003ffa5b4c0456
Zach777 <MeteorCity3109@protonmail.com>
2020-10-08 21:08:22 -0400

    Wip

--- Script/Manager/Helpers.gd
+++ Script/Manager/Helpers.gd
@@ -1,7 +1,17 @@
 extends Node
 # Common helper functions
 
+const SAVE_LOCATION : String = "user://Moonwards/user_settings.ini"
+
 var Enum = EnumHelper.new()
+
+#The file that the player uses.
+var user_file : ConfigFile = ConfigFile.new()
+
+#These are the actions that are editable.
+var editable_actions : Array = [
+	"move_forwards", "move_backwards"
+]
 
 var is_capture_mode : bool = false
 
@@ -12,6 +22,11 @@ func _input(event):
 		else:
 			capture_mouse(true)
 
+#Load the user file. Create it if it does not exist.
+func _ready() -> void :
+	load_user_file()
+
+#Decide whether the mouse should be captured or not.
 func capture_mouse(capture_mouse : bool) -> void :
 	if capture_mouse == true :
 		is_capture_mode = true
@@ -28,11 +43,64 @@ func center_mouse() -> void :
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func reparent(node, new_parent, keep_world_pos = false):
-	var pos = node.global_transform.origin
-	var p = node.get_parent()
-	p.remove_child(node)
-	new_parent.add_child(node)
-	node.owner = new_parent
-	if keep_world_pos:
-		node.global_transform.origin = pos
+#Get an action from the InputMap. Returns string for InputEventType and it's action identifier.
+func get_action(action_name : String) -> Array :
+	var event : InputEvent = InputMap.get_action_list(action_name)[0]
+	var event_type : String = "SomethingWentWrong"
+	var scancode : int = 0
+	if event is InputEventKey :
+		scancode = event.scancode
+		event_type = "Key"
+	elif event is InputEventMouseButton : 
+		scancode = event.button_index
+		event_type = "MouseButton"
+	
+	return [event_type, scancode] 
+
+#Load the user file which stores all the player's personal settings.
+func load_user_file() -> void :
+	#Create the user file if it does not already exist.
+	var dir : Directory = Directory.new()
+	var is_default : bool = false #Don't recreate Events when using default.
+	if not dir.file_exists(SAVE_LOCATION) :
+		dir.make_dir("user://Moonwards/")
+#		var file : File = File.new()
+#		file.open(SAVE_LOCATION, file.WRITE)
+#
+#		for action in editable_actions :
+#			var array : Array = get_action(action)
+#			file.store_string(("[%s]" % action) + "\n") #Section
+#			file.store_string(("event_type=\"%s\"" % array[0]) + "\n") 
+#			file.store_string(("scancode=%s" % array[1]) + "\n")
+#
+#		file.close()
+#		is_default = true
+	
+	#Load the user file.
+	user_file.load(SAVE_LOCATION)
+	
+	#Don't recreate actions if it is a default user file.
+	if is_default :
+		return
+	
+	#Start creating the InputEvents needed.
+	for action in editable_actions :
+		var event : InputEvent
+		var type : String = user_file.get_value(action, "event_type")
+		if type == "Key" :
+			event = InputEventKey.new()
+			event.scancode = user_file.get_value(action, "scancode")
+		elif type == "MouseButton" :
+			event = InputEventMouseButton.new()
+			event.button_index = user_file.get_value(action, "scancode")
+		
+		
+
+#Save user info to the file.
+func save_user_file() -> void :
+	for action in editable_actions :
+		var array = get_action(action)
+		user_file.set_value(action, "event_type", array[0])
+		user_file.set_value(action, "scancode", array[1])
+	
+	user_file.save(SAVE_LOCATION)
