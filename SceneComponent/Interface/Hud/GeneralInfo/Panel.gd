93aaff6c48afff3e53d02302b8a4baee2cbf334c
Zach777 <MeteorCity3109@protonmail.com>
2020-10-15 21:54:48 -0400

    New buttons at top right. Panel has buttons now

--- SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
@@ -1,8 +1,8 @@
-extends Panel
+extends PanelContainer
 
 
 func _ready() -> void :
-	get_parent().get_node("HBoxContainer3/Button").connect("pressed", self, "_toggle")
+	get_parent().get_node("TopRight/Button").connect("pressed", self, "_toggle")
 
 func _toggle() -> void :
 	visible = !visible
