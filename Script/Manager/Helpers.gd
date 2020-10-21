866593cc96040320d928597211be6d512d02c622
Zach777 <MeteorCity3109@protonmail.com>
2020-10-20 21:43:38 -0400

    Added actions to Custom Controls

--- Script/Manager/Helpers.gd
+++ Script/Manager/Helpers.gd
@@ -11,7 +11,8 @@ var user_file : ConfigFile = ConfigFile.new()
 #These are the actions that are editable.
 var editable_actions : Array = [
 	"move_forwards", "move_backwards", "move_left", "move_right",
-	"jump", "interact_with_closest", "use"
+	"jump", "interact_with_closest", "use", "toggle_first_person",
+	"fly_up", "fly_down"
 ]
 
 var is_capture_mode : bool = false
