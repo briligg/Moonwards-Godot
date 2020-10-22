43b7bd84b26c112f555fb8b328d9d2514626195e
Zach777 <MeteorCity3109@protonmail.com>
2020-10-22 08:56:24 -0400

    Hide menu panel when app changes - Make Panel able to be selected by the mouse.

---
+++ SceneComponent/Interface/Hud/GeneralInfo/GeneralInfo.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func hide_panels(_old_app = null, _new_app = null) -> void :
+	$Panel.hide_panel()
--- SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
@@ -1,6 +1,8 @@
 extends PanelContainer
 
 
+func hide_panel() -> void :
+	hide()
 
 func toggle_menu_panel() -> void :
 	visible = !visible
