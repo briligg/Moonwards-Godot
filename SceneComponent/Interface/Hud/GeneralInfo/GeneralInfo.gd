29540990acb1f17d762938f51cbffa95927bdd54
Zach777 <MeteorCity3109@protonmail.com>
2020-10-23 23:02:42 -0400

    Create a help menu for future development

--- SceneComponent/Interface/Hud/GeneralInfo/GeneralInfo.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/GeneralInfo.gd
@@ -2,4 +2,5 @@ extends Control
 
 
 func hide_panels(_old_app = null, _new_app = null) -> void :
-	$Panel.hide_panel()
+	$MenuPanel.hide_panel()
+	$HelpPanel.hide_panel()
--- SceneComponent/Interface/Hud/GeneralInfo/HelpPanel.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/HelpPanel.gd
@@ -1,6 +1,10 @@
 extends Panel
 
 
+#Hide the panel. Clean up anything that needs cleaning up.
+func hide_panel() -> void :
+	hide()
+
 #Toggle the help menu on or off.
 func toggle_help_menu() -> void :
 	visible = !visible
