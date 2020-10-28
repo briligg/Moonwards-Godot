0fe34eb60caad25e0fe6a30e86a46e20d863fd71
Zach777 <MeteorCity3109@protonmail.com>
2020-10-27 22:06:30 -0400

    wip

--- SceneComponent/Interface/Hud/Apps/Building/BuildingApp.gd
+++ SceneComponent/Interface/Hud/Apps/Building/BuildingApp.gd
@@ -4,7 +4,7 @@ extends PanelContainer
 #Listen to when we should close the BuildingApp.
 func _ready() -> void :
 	var exit_button : Button
-	exit_button = $VBoxContainer/HBoxContainer2/VBoxContainer2/MarginContainer2/Button_Exit
+	exit_button = $BottomButtons/VBoxContainer2/Button_Exit
 	exit_button.connect("pressed", self, "exit")
 
 func exit() -> void :
