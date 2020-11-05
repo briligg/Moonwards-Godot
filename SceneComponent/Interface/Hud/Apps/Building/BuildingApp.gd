f77096feee67c2c1407d5b2b3d73a26b18115a44
Zach777 <MeteorCity3109@protonmail.com>
2020-11-05 12:59:06 -0500

    Building App buttons larger #603 (#705)
    * wip
    
    * Enlarge building app buttons
    - Remove graphical glitches
    
    * Adjust positioning of panel
    
    * Center and enlarge button icons

--- SceneComponent/Interface/Hud/Apps/Building/BuildingApp.gd
+++ SceneComponent/Interface/Hud/Apps/Building/BuildingApp.gd
@@ -1,10 +1,10 @@
-extends PanelContainer
+extends Control
 
 
 #Listen to when we should close the BuildingApp.
 func _ready() -> void :
 	var exit_button : Button
-	exit_button = $VBoxContainer/HBoxContainer2/VBoxContainer2/MarginContainer2/Button_Exit
+	exit_button = $BottomButtons/VBoxContainer2/Button_Exit
 	exit_button.connect("pressed", self, "exit")
 
 func exit() -> void :
