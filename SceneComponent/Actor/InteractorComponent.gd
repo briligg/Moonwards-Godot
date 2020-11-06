287a59aa810bba86614540dea25adc8d13a0b964
Zach777 <MeteorCity3109@protonmail.com>
2020-11-05 22:03:33 -0500

    Unique text for Interacts Menu only Interactors

--- SceneComponent/Actor/InteractorComponent.gd
+++ SceneComponent/Actor/InteractorComponent.gd
@@ -46,7 +46,8 @@ func _make_hud_display_interactable(interactable : Interactable = null) -> void 
 	if interactable == null :
 		Signals.Hud.emit_signal(Signals.Hud.INTERACTABLE_DISPLAY_HIDDEN)
 	else :
-		Signals.Hud.emit_signal(Signals.Hud.INTERACTABLE_DISPLAY_SHOWN, interactable.title)
+		Signals.Hud.emit_signal(Signals.Hud.INTERACTABLE_DISPLAY_SHOWN, 
+				interactable.title, disable_ray_cast)
 
 #Make Interactor have my Entity variable as it's user.
 func _ready() -> void :
@@ -111,6 +112,11 @@ func grab_focus() -> void:
 	#Display what is the closest interactable.
 	interactor_ray.connect("new_interactable", self, "_make_hud_display_interactable")
 	interactor_ray.connect("no_interactable_in_reach", self, "_make_hud_display_interactable")
+	
+	#Let the InteractsDisplay know if I only use InteractsMenu.
+	if disable_ray_cast :
+		Signals.Hud.emit_signal(Signals.Hud.INTERACTABLE_DISPLAY_SHOWN, 
+				"", disable_ray_cast)
 
 #Another interactor has grabbed focus. Perform cleanup.
 func lost_focus() -> void :
--- SceneComponent/Interface/Hud/InteractDisplay/InteractDisplay.gd
+++ SceneComponent/Interface/Hud/InteractDisplay/InteractDisplay.gd
@@ -11,11 +11,16 @@ func _ready() -> void :
 	Signals.Hud.connect(Signals.Hud.INTERACTABLE_DISPLAY_HIDDEN, self, "_hide_interact_info")
 
 #Let the user know what interaction is possible.
-func _show_interact_info(interactable_title : String) -> void :
-	#Hide the menu if there are no Interactables to interact with.
-	text = "Press " 
-	text += OS.get_scancode_string(InputMap.get_action_list("use")[0].scancode)
-	text += " to show InteractsMenu.\n"
-	text += "Press " + OS.get_scancode_string(InputMap.get_action_list("interact_with_closest")[0].scancode)
-	text += " to interact with " + interactable_title
+func _show_interact_info(interactable_title : String, uses_menu_only : bool) -> void :
 	show()
+	
+	#If you cannot interact via a Ray Cast then 
+	#let the player know to bring up the menu.
+	if uses_menu_only :
+		text = "Press " 
+		text += OS.get_scancode_string(InputMap.get_action_list("use")[0].scancode)
+		text += " to show InteractsMenu."
+		return
+	
+	text = "Press " + OS.get_scancode_string(InputMap.get_action_list("interact_with_closest")[0].scancode)
+	text += " to interact with " + interactable_title
