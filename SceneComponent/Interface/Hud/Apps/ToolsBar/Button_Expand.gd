0ee62ae3bb32372a81311427c127cd816ae87db6
Zach777 <MeteorCity3109@protonmail.com>
2020-11-06 16:32:01 -0500

    Player start UX improvement #736 (#754)
    * Log Console is hidden at start
    
    * Remove expand button double press bug
    
    * ToolsBar starts in minimized state
    
    * SceneManager captures mouse when loading world

--- SceneComponent/Interface/Hud/Apps/ToolsBar/Button_Expand.gd
+++ SceneComponent/Interface/Hud/Apps/ToolsBar/Button_Expand.gd
@@ -1,16 +1,9 @@
 extends Button
 
 
-#Hack to work around a bug where buttons are being pressed twice.
-var hack_two_press_bug : bool = true
-
-var is_collapsed : bool = false
+var is_collapsed : bool = true
 
 func _pressed() -> void :
-	if hack_two_press_bug :
-		hack_two_press_bug = false
-		return
-	
 	#Make it so I rotate perfectly.
 	var texture : TextureRect = get_node("../TextureRect")
 	texture.rect_pivot_offset = texture.rect_size * 0.5
@@ -25,7 +18,3 @@ func _pressed() -> void :
 		dynamic_anim.play("hide")
 	
 	is_collapsed = !is_collapsed
-	hack_two_press_bug = true
-
-func _ready() -> void :
-	connect("pressed", self, "_pressed")
