5c3c74d838d3338f5850e6f2324402457603010a
Zach777 <MeteorCity3109@protonmail.com>
2020-11-11 21:13:05 -0500

    Add close button and text to Tutorial Menu

--- SceneComponent/Interface/TutorialMenu/TutorialMenu.gd
+++ SceneComponent/Interface/TutorialMenu/TutorialMenu.gd
@@ -3,6 +3,14 @@ extends Panel
 #I am part of group TutorialMenu
 
 
+#Close pressed.
+func _close() -> void :
+	visible = false
+
+#Close the tutorial menu if closed is pressed.
+func _ready() -> void :
+	$Close.connect("pressed", self, "_close")
+
 #Toggles the Tutorial Menus visibility.
 func toggle() -> void :
 	visible = !visible
