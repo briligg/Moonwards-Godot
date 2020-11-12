dd98e36fa983c390d1921d76513280f9e4c8bc11
Zach777 <MeteorCity3109@protonmail.com>
2020-11-12 09:36:36 -0500

    Take into account custom controls in tutorial text

---
+++ SceneComponent/Interface/TutorialMenu/TextFormatter.gd
@@ -0,0 +1,13 @@
+extends VBoxContainer
+
+
+func _ready() -> void :
+	$Text.text = $Text.text % [
+		Helpers.get_action_string("move_forwards"),
+		Helpers.get_action_string("move_left"),
+		Helpers.get_action_string("move_backwards"),
+		Helpers.get_action_string("move_right"),
+		Helpers.get_action_string("jump"),
+		Helpers.get_action_string("toggle_first_person"),
+		Helpers.get_action_string("interact_with_closest")
+	]
