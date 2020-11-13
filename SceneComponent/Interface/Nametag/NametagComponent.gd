039fd8468733fac758bae933d40f3af166da1be9
Zach777 <MeteorCity3109@protonmail.com>
2020-11-13 17:43:30 -0500

    Nametags appear over other player's heads'

--- SceneComponent/Interface/Nametag/NametagComponent.gd
+++ SceneComponent/Interface/Nametag/NametagComponent.gd
@@ -4,7 +4,10 @@ func _init().("NametagComponent", false):
 	pass
 	
 func _ready() -> void:
-	set_name(entity.entity_name)
+	call_deferred("set_name", entity.entity_name)
+	
+	if not is_net_owner() :
+		visible = true
 	
 func set_name(name) -> void:
 	$Nametag3D.set_name(name)
