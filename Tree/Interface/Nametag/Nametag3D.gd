254fb17758acbd7302ef014672acfedb4f9492f0
Zach777 <MeteorCity3109@protonmail.com>
2020-11-13 22:18:08 -0500

    Add comments to Nametag3D.gd

--- Tree/Interface/Nametag/Nametag3D.gd
+++ Tree/Interface/Nametag/Nametag3D.gd
@@ -4,9 +4,11 @@
 extends MeshInstance
 
 
+#Only call ready once even if we get moved around the scene tree.
 func _init() -> void :
 	call_deferred("_ready_once")
 
+#We have to do this in order for all Nametags to properly display the name.
 func _ready_once() -> void :
 	material_override.albedo_texture = $NametagHolder.get_texture()
 
