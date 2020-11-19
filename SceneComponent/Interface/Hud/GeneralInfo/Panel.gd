3878522c3d6c51b99d0e9a2e8b456805251bde6c
Zach777 <MeteorCity3109@protonmail.com>
2020-11-18 21:46:06 -0500

    Add Custom Controls to Menu Panel Wip

--- SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
@@ -1,5 +1,11 @@
-extends Panel
+extends PanelContainer
 
+
+func controls_pressed() -> void :
+	var vbox : VBoxContainer = $VBoxContainer
+	vbox.visible = !vbox.visible
+	var controls : Tabs = $Controls
+	controls.visible = !controls.visible
 
 func hide_panel() -> void :
 	hide()
