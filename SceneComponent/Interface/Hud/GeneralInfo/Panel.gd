48d88e0c91c0959a4e7603b385d5ed8ff903179e
Zach777 <MeteorCity3109@protonmail.com>
2020-11-18 22:09:28 -0500

    Menu Panel switches between submenus

--- SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
@@ -1,14 +1,32 @@
-extends PanelContainer
+extends Panel
 
+onready var back : TextureButton = $Back
+
+
+func _back_pressed() -> void :
+	if not $VBoxContainer.visible :
+		back.hide()
+		controls_pressed()
+
+#Listen for back being pressed so we can hide it.
+func _ready() -> void :
+	back.connect("pressed", self, "_back_pressed")
 
 func controls_pressed() -> void :
 	var vbox : VBoxContainer = $VBoxContainer
 	vbox.visible = !vbox.visible
 	var controls : Tabs = $Controls
 	controls.visible = !controls.visible
+	
+	#Let the player go back if they need to.
+	if controls.visible :
+		back.show()
 
 func hide_panel() -> void :
 	hide()
 
 func toggle_menu_panel() -> void :
 	visible = !visible
+
+
+
