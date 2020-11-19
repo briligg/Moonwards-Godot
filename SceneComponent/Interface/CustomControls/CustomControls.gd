dc5c1c7573e8a6355033bc5a487cd69843a656fb
Zach777 <MeteorCity3109@protonmail.com>
2020-11-19 10:34:54 -0500

    Refactor setup of Custom Controls

--- SceneComponent/Interface/CustomControls/CustomControls.gd
+++ SceneComponent/Interface/CustomControls/CustomControls.gd
@@ -1,22 +1,23 @@
-extends Tabs
+extends Control
 
 
 func _defaults_pressed() -> void :
 	InputMap.load_from_globals()
 	
 	#Go through all control buttons and set them to default.
-	for child in $ButtonList/HBox/Sub1.get_children() :
+	for child in $HBox/Sub1.get_children() :
 		if child is HBoxContainer :
 			child.update_labels()
-	for child in $ButtonList/HBox/Sub2.get_children() :
+	for child in $HBox/Sub2.get_children() :
 		if child is HBoxContainer :
 			child.update_labels()
 	
-	_on_Button_pressed()
-
-func _on_Button_pressed():
-	Helpers.save_user_file()
+	save_pressed()
 
 func _ready() -> void :
-	var button : Button = $ButtonList/HBox/Sub1/DefaultControls
+	var button : Button = $HBox/Sub1/DefaultControls
 	button.connect("pressed", self, "_defaults_pressed")
+
+
+func save_pressed():
+	Helpers.save_user_file()
