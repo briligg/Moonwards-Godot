97b636c819265353cda013ba0b36d7589e85cb7c
Zach777 <MeteorCity3109@protonmail.com>
2020-12-01 10:42:19 -0500

    Add close button to Help Menu (#816)

--- SceneComponent/Interface/Hud/GeneralInfo/HelpPanel.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/HelpPanel.gd
@@ -2,10 +2,14 @@ extends Panel
 
 onready var help_menu : Control = $Alignment/HelpMenu
 onready var back_button : TextureButton = $Alignment/TopSection/Label/BackButton
+onready var close : TextureButton = get_node("Alignment/TopSection/Label/Close")
 
 #Listen to back button presses.
 func _ready() -> void :
 	back_button.connect("pressed", self, "back_pressed")
+	
+	#Close when the close button is pressed.
+	close.connect("pressed", self, "toggle_help_menu")
 
 func back_pressed() -> void :
 	var menus : Control = $Alignment/HelpMenu
@@ -16,6 +20,8 @@ func hide_panel() -> void :
 	help_menu.back_to_start()
 	back_button.hide()
 	hide()
+	
+	
 
 #Toggle the help menu on or off.
 func toggle_help_menu() -> void :
