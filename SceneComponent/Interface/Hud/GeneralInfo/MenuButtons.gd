8e7a4cbc5fbaa1d4e02d6fddf8b1a3ed83c156f4
Zach777 <MeteorCity3109@protonmail.com>
2020-12-09 10:37:38 -0500

    Add Custom Controls to Menu Panel #823 (#824)
    * Menu Panel uses a proper panel theme
    
    * Make Menu Panel it's own scene
    
    * Make it convenient to open and edit MenuPanel.tscn
    
    * Add Custom Controls to Menu Panel Wip
    
    * Menu Panel switches between submenus
    
    * Initial creation of animation player in Menu Panel
    
    * Refactor setup of Custom Controls
    
    * Animate opening of Custom Controls in Menu Panel
    
    * Add close button to Menu Panel
    
    * Godot reformated Custom Controls automatically
    
    * Fix mouse toggle bug when setting custom control

--- SceneComponent/Interface/Hud/GeneralInfo/MenuButtons.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/MenuButtons.gd
@@ -8,5 +8,6 @@ func _quit_pressed() -> void :
 	get_tree().quit()
 
 func _ready() -> void :
+	$Controls.connect("pressed", get_parent(), "controls_pressed")
 	$MainMenu.connect("pressed", self, "_main_menu_pressed")
 	$Quit.connect("pressed", self, "_quit_pressed") 
--- SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
+++ SceneComponent/Interface/Hud/GeneralInfo/Panel.gd
@@ -1,8 +1,34 @@
-extends PanelContainer
+extends Panel
 
+onready var back : TextureButton = $Back
+
+
+func _back_pressed() -> void :
+	if not $VBoxContainer.visible :
+		controls_pressed()
+		$Anim.play_backwards("Controls")
+		$AnimVisibility.play("ShowMainSubmenu")
+
+#Listen for back being pressed so we can hide it. Close panel if close is pressed.
+func _ready() -> void :
+	back.connect("pressed", self, "_back_pressed")
+	$Close.connect("pressed", self, "hide_panel")
+
+func controls_pressed() -> void :
+	var vbox : VBoxContainer = $VBoxContainer
+	vbox.visible = !vbox.visible
+	var controls : Control = $Controls
+	
+	#Showing Controls menu.
+	if not controls.visible :
+		$Anim.play("Controls")
+		$AnimVisibility.play("ShowControls")
 
 func hide_panel() -> void :
 	hide()
 
 func toggle_menu_panel() -> void :
 	visible = !visible
+
+
+
