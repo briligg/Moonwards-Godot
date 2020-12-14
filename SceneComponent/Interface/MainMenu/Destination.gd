acd5b528c27d4dcd6e38633d2d142ff943c658f6
yael <yael.atletl@gmail.com>
2020-12-14 13:20:16 -0600

    change labels to easier to understand terms
    also move buttons around

--- SceneComponent/Interface/MainMenu/Destination.gd
+++ SceneComponent/Interface/MainMenu/Destination.gd
@@ -6,8 +6,8 @@ signal joined_master()
 signal joined_server(server_ip_address_string)
 signal started_custom_server()
 
-onready var ip_address : TextEdit = $HBoxContainer/VBoxContainer/VBoxContainer/TextEdit_Address
-onready var port_text : TextEdit = $HBoxContainer/VBoxContainer/VBoxContainer/TextEdit_Port
+onready var ip_address : TextEdit = $HBoxContainer/VBoxContainer/PanelContainer3/VBoxContainer/TextEdit_Address
+onready var port_text : TextEdit = $HBoxContainer/VBoxContainer/PanelContainer3/VBoxContainer/TextEdit_Port
 #What the main server IP is.
 const MAIN_SERVER_IP : String = "45.77.1.84"
 const DEFAULT_PORT : int = 5000
@@ -30,13 +30,3 @@ func _started_custom_server() -> void :
 	emit_signal("started_custom_server")
 	Signals.Network.emit_signal(Signals.Network.GAME_SERVER_REQUESTED, DEFAULT_PORT)
 
-func _ready() -> void :
-	var button : Button
-	button = $HBoxContainer/VBoxContainer/Button_JoinServer
-	button.connect("pressed", self, "_joined_server")
-	
-	button = $HBoxContainer/VBoxContainer2/Button_JoinMaster
-	button.connect("pressed", self, "_joined_master")
-	
-	button = $HBoxContainer/VBoxContainer2/Button_CreateServer
-	button.connect("pressed", self, "_started_custom_server")
