81628eb3c50e4dc2dcf318b7d6b944774cfdd781
Zach777 <MeteorCity3109@protonmail.com>
2020-12-16 22:14:41 -0500

    Add social media links to Main Menu #921 (#922)
    * Add social media links to Main Menu
    
    * Make media buttons have Moonwards button styling
    
    * Add source code link

---
+++ Tree/Interface/MainMenu/Links.gd
@@ -0,0 +1,11 @@
+extends HBoxContainer
+
+
+func _ready() -> void :
+	$Source.connect("pressed", self, "_link_pressed", ["https://github.com/moonwards1/Moonwards-Virtual-Moon"])
+	$Twitter.connect("pressed", self, "_link_pressed", ["https://twitter.com/moonwards1"])
+	$Reddit.connect("pressed", self, "_link_pressed", ["https://www.reddit.com/r/Moonwards/"])
+	$Discord.connect("pressed", self, "_link_pressed", ["https://discord.gg/QHr62v8"])
+
+func _link_pressed(link : String) -> void :
+	OS.shell_open(link)
