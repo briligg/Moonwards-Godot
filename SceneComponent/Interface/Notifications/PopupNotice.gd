083558338b97ce56f5edd414e2bdf7002ece3bfa
Zach777 <MeteorCity3109@protonmail.com>
2020-12-24 19:52:25 -0500

    Fix links in messages crashing the game (#953)

--- SceneComponent/Interface/Notifications/PopupNotice.gd
+++ SceneComponent/Interface/Notifications/PopupNotice.gd
@@ -2,9 +2,14 @@ extends CanvasLayer
 
 func _ready() -> void  :
 	$CenterNotice/Button.connect("pressed", self, "_pressed")
+	$CenterNotice/Text.connect("meta_clicked", self, "_link_clicked")
 
 func _pressed() -> void :
 	self.queue_free()
 
 func set_text(new_text : String) -> void :
+	new_text = Helpers.setup_text_with_links(new_text)
 	$CenterNotice/Text.bbcode_text = new_text
+
+func _link_clicked(link_text : String) -> void :
+	OS.shell_open(link_text)
