39c18130c64dc777b1f3709c7da713ef21f0e1d0
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-12-28 20:14:28 +0200

    Fix version displayed on the main menu

--- Tree/Interface/MainMenu/Links.gd
+++ Tree/Interface/MainMenu/Links.gd
@@ -6,6 +6,7 @@ func _ready() -> void :
 	$Twitter.connect("pressed", self, "_link_pressed", ["https://twitter.com/moonwards1"])
 	$Reddit.connect("pressed", self, "_link_pressed", ["https://www.reddit.com/r/Moonwards/"])
 	$Discord.connect("pressed", self, "_link_pressed", ["https://discord.gg/QHr62v8"])
-
+	$Label_Version.text = "Game Version %s     Sites:  " %WorldConstants.VERSION
+	
 func _link_pressed(link : String) -> void :
 	OS.shell_open(link)
