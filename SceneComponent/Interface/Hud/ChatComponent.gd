36ba27ff14f8e265bbc6e9d1e5e5c2322f9ac8de
Zach777 <MeteorCity3109@protonmail.com>
2021-03-02 21:38:18 -0500

    Server shares chat history with joining players

--- SceneComponent/Interface/Hud/ChatComponent.gd
+++ SceneComponent/Interface/Hud/ChatComponent.gd
@@ -1,11 +1,17 @@
 extends MwSpatial
 onready var chatbox = $Chat
 
+#History of chat messages.
+var history : PoolStringArray = PoolStringArray()
+
 func _init():
 	pass
 
 func _ready() -> void:
 	chatbox.connect("chat_line_entered", self, "_chat_line_entered")
+	
+	#Give client chat the message history.
+	Signals.Network.connect(NetworkSignals.NEW_PLAYER_POST_LOAD, self, "sync_new_chat")
 
 func _chat_line_entered(message: String, _strength: int) -> void:
 	rpc_id(1, "_send_chat_message", message)
@@ -18,6 +24,15 @@ master func _send_chat_message(message: String) -> void:
 	var m = "%s: %s" %[e.entity_name, message]
 	Log.trace(self, "", "Sending chat message: %s" % m)
 	crpc("_receive_chat_message", m)
+	
+	history.append(m)
+
+master func sync_new_chat(chat_id : int) -> void :
+	rpc_id(chat_id, "client_chat_sync", history)
+
+puppetsync func client_chat_sync(sync_history : PoolStringArray) -> void :
+	for message in sync_history :
+		chatbox.add_message(message)
 
 #`PUPPETSYNC`
 # Runs on the clients
