f9a2a273e8e1289d5414859b7d5b79aaf746f128
Kim <moonwards1@gmail.com>
2021-03-11 12:55:39 -0600

    Line up passage to North Farm Atrium with floor of FGU

--- Script/Manager/SceneManager/SceneManager.gd
+++ Script/Manager/SceneManager/SceneManager.gd
@@ -4,6 +4,7 @@ signal scene_changed
 
 ### Temporary, until proper scene management is implemented.
 #var world_scene: String = "res://_tests/NewDemo/TestScene.tscn"
+<<<<<<< HEAD
 #var world_scene: String = "res://Tree/World/Moon_Town_Main.tscn"
 #var world_scene: String = "res://_tests/Display/BaseDisplayTest.tscn"
 var world_scene: String = "res://Tree/World/Moon_Town_Main.tscn"
