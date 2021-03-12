ba0b255cebc9c615c91774baa29f3c42f9cb08a3
Zach777 <MeteorCity3109@protonmail.com>
2021-03-12 09:08:36 -0500

    Make MoonTownMain the world scene in SceneManager

--- Script/Manager/SceneManager/SceneManager.gd
+++ Script/Manager/SceneManager/SceneManager.gd
@@ -5,8 +5,8 @@ signal scene_changed
 ### Temporary, until proper scene management is implemented.
 #var world_scene: String = "res://_tests/NewDemo/TestScene.tscn"
 #var world_scene: String = "res://Tree/World/Moon_Town_Main.tscn"
-var world_scene: String = "res://_tests/Display/BaseDisplayTest.tscn"
-#var world_scene: String = "res://Tree/World/Moon_Town_Main.tscn"
+#var world_scene: String = "res://_tests/Display/BaseDisplayTest.tscn"
+var world_scene: String = "res://Tree/World/Moon_Town_Main.tscn"
 #var world_scene: String = "res://_tests/RoverTest.tscn"
 #var world_scene : String = "res://_tests/Display/RocketDisplayTest.tscn"
 var main_menu: String = "res://Tree/Interface/MainMenu/MainMenu.tscn"
