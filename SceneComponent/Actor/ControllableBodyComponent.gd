27262cea13dde60578402e632818b63a8cd3e4a8
Zach777 <MeteorCity3109@protonmail.com>
2021-03-15 09:30:24 -0400

    Sync controllable body components for new players

--- SceneComponent/Actor/ControllableBodyComponent.gd
+++ SceneComponent/Actor/ControllableBodyComponent.gd
@@ -32,20 +32,13 @@ func _ready() -> void :
 		$Interactable.set_active(false)
 	
 	$Interactable.connect("interacted_by", self, "_been_interacted")
+	$Interactable.connect("sync_for_new_player", self, "sync_for_new_player")
 
 func _been_interacted(interactor : Node) -> void :
 	#Return control to human player.
 	if  interactor.owner_peer_id == self.controller_peer_id :
 		#Disable my entity and get interactor ready.
-		entity.disable()
-		controlling_entity.enable()
-
-		#Reset myself.
-		entity.get_component("Interactor").disable()
-		entity.get_component("HumanoidInput").disable()
-		entity.owner_peer_id = original_peer_id
-		controller_peer_id = original_peer_id
-		is_being_controlled = false
+		rpc("_sync_return_control")
 
 		#Hand control back to the original actor.
 		entity.get_component("Camera").camera.current = false
@@ -69,21 +62,42 @@ func _been_interacted(interactor : Node) -> void :
 	if interactor.has_node("HumanoidInput") :
 		interactor.get_node("HumanoidInput").disable()
 	
-	entity.owner_peer_id = interactor.owner_peer_id
-	controller_peer_id = interactor.owner_peer_id
-	
-	interactor.disable()
-	controlling_entity = interactor
 	entity.get_component("Camera").camera.current = true
 	entity.get_component("Interactor").grab_focus()
-	entity.enable()
-	is_being_controlled = true
+	crpc("_sync_take_control", interactor.get_path())
 	
 	VisibilityManager.switch_context()
 	
 	#Handle when the player disconnects
 	var net : NetworkSignals = Signals.Network
 	net.connect(net.CLIENT_DISCONNECTED, self, "_client_disconnected")
+
+sync func _sync_return_control() -> void :
+	entity.disable()
+	controlling_entity.enable()
+	controlling_entity.show()
+	
+	entity.get_component("NametagComponent").hide()
+	entity.get_component("Interactor").disable()
+	entity.get_component("HumanoidInput").disable()
+	entity.owner_peer_id = original_peer_id
+	controller_peer_id = original_peer_id
+	is_being_controlled = false
+
+#Called when someone else takes control of me.
+sync func _sync_take_control(interactor_path : NodePath) -> void :
+	var interactor = get_tree().get_root().get_node(interactor_path)
+	#Give control to the new interactor..
+	entity.owner_peer_id = interactor.owner_peer_id
+	controller_peer_id = interactor.owner_peer_id
+	
+	interactor.disable()
+	interactor.hide()
+	controlling_entity = interactor
+	entity.get_component("NametagComponent").show()
+	entity.get_component("NametagComponent").set_name(interactor.entity_name)
+	entity.enable()
+	is_being_controlled = true
 
 func _client_disconnected(peer_id) -> void :
 	#If controlling entity disconnects, return control to normal.
@@ -92,3 +106,15 @@ func _client_disconnected(peer_id) -> void :
 
 func interact_with(aentity : AEntity) -> void :
 	$Interactable.interact_with(aentity)
+
+func sync_for_new_player(peer_id) -> void :
+	if not controlling_entity == null :
+		rpc_id(peer_id, "sync_to_master", controlling_entity.get_path())
+	else :
+		rpc_id(peer_id, "sync_to_master", null)
+
+puppet func sync_to_master(entity_path : NodePath) -> void :
+	if not entity_path == null :
+		controlling_entity = get_tree().get_root().get_node(entity_path)
+		_sync_take_control(entity_path)
+		is_being_controlled = true
