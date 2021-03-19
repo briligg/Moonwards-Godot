991cd0fe53e49ca532c20f03309ecf05d7d41dec
Zach777 <MeteorCity3109@protonmail.com>
2021-03-19 19:05:27 -0400

    Prevent spacesuits from being able to climb stairs

--- SceneComponent/Actor/Humanoid/HumanoidMovement.gd
+++ SceneComponent/Actor/Humanoid/HumanoidMovement.gd
@@ -6,6 +6,7 @@ export(float) var initial_jump_velocity = 2.2
 export(float) var climb_speed = 1.5
 export(float) var movement_speed = 3
 export(float) var gravity = 1.625
+export(bool) var can_climb_stairs = true
 
 onready var on_ground : Node = $OnGround
 onready var normal_detect : Node = $NormalDetect
@@ -245,7 +246,7 @@ func disable_ground_cast_for_seconds(duration = 0.0):
 # to be moved elsewhere and possibly reworked.
 func start_climb_stairs(target_stairs : VerticalStairs) -> void:
 	#Do nothing if the player is already in climbing state.
-	if is_climbing:
+	if is_climbing || not can_climb_stairs :
 		return
 	
 	#Disable entity collision so that no issues happen with multiple players
