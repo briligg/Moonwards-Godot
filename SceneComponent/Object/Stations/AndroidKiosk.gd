00dae8c8db673d212f84bfc3ba2adfb71ecca89c
Zach777 <MeteorCity3109@protonmail.com>
2021-03-26 09:01:43 -0400

    Initial creation of Android kiosk in Airlock Bay

---
+++ SceneComponent/Object/Stations/AndroidKiosk.gd
@@ -0,0 +1,13 @@
+extends Spatial
+
+
+export(NodePath) var android_to_control 
+
+
+func _interaction(interactor : AEntity) -> void :
+	var comp : InteractorComponent =interactor.get_component("Interactor")
+	var android : AEntity = get_node(android_to_control)
+	comp.player_requested_interact(android.get_node("ControllableBodyComponent/Interactable"))
+
+func _ready() -> void :
+	$Interactable.connect("interacted_by", self, "_interaction")
