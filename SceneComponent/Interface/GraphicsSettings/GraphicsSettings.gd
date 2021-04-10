be0126d02a09a1967cabafd39c1d712152690758
Zach777 <MeteorCity3109@protonmail.com>
2021-04-10 10:47:00 -0400

    Add working high graphics settings

--- SceneComponent/Interface/GraphicsSettings/GraphicsSettings.gd
+++ SceneComponent/Interface/GraphicsSettings/GraphicsSettings.gd
@@ -7,7 +7,47 @@ var list : Array = ProjectSettings.get_property_list()
 
 
 func _high_selected() -> void :
-	pass
+	_pj_set(QUALITY + "intended_usage/framebuffer_allocation", 2)
+	
+	var text : String = QUALITY + "filters/"
+	_pj_set(text + "anisotropic_filter_level", 16)
+	_pj_set(text+"use_nearest_mipmap_filter", true)
+	_pj_set(text+"msaa", 1)
+	
+	text = QUALITY+"directional_shadow/"
+	_pj_set(text+"size", 4096)
+	
+	text = QUALITY+"shadow_atlas/quadrant_"
+	_pj_set(text+"0_subdiv", 1)
+	_pj_set(text+"1_subdiv", 2)
+	_pj_set(text+"2_subdiv", 3)
+	_pj_set(text+"3_subdiv", 4)
+	text = QUALITY+"shadow_atlas/"
+	_pj_set(text+"size", 4096)
+	
+	text = QUALITY + "shadows/"
+	_pj_set(text+"filter_mode", 2)
+	
+	text = QUALITY + "reflections/"
+	_pj_set(text+"texture_array_reflections", true)
+	_pj_set(text+"high_quality_ggx", true)
+	_pj_set(text+"irradiance_max_size", 256)
+	_pj_set(text+"atlas_size", 2048)
+	_pj_set(text+"atlas_subdiv", 8)
+	
+	text = QUALITY+"shading/"
+	_pj_set(text+"force_vertex_shading", false)
+	_pj_set(text+"force_lambert_over_burley", false)
+	
+	text = QUALITY+"depth_prepass/"
+	_pj_set(text+"enable", true)
+	
+	text = QUALITY+"subsurface_scattering/"
+	_pj_set(text+"quality", 1)
+	_pj_set(text+"scale", 1)
+	
+	text = QUALITY+"depth/"
+	_pj_set(text+"hdr", true)
 
 func _ready() -> void :
 	var graphics = $LodTransition2/GraphicsOptions
