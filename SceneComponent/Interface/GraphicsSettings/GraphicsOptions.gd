b267881d597279c5004758a20f9a13db2f7855ea
Zach777 <MeteorCity3109@protonmail.com>
2021-04-10 09:58:56 -0400

    Graphics settings can be toggled to low

---
+++ SceneComponent/Interface/GraphicsSettings/GraphicsOptions.gd
@@ -0,0 +1,19 @@
+extends OptionButton
+
+
+signal low_selected()
+signal high_selected()
+
+
+func _item_selected(index : int) -> void :
+	if index == 0 :
+		emit_signal("low_selected")
+	else :
+		emit_signal("high_selected")
+
+#Setup the options button and handle toggling options.
+func _ready() -> void :
+	add_item("Low", 0)
+	add_item("High", 1)
+	
+	connect("item_selected", self, "_item_selected")
--- SceneComponent/Interface/GraphicsSettings/GraphicsSettings.gd
+++ SceneComponent/Interface/GraphicsSettings/GraphicsSettings.gd
@@ -1,1 +1,65 @@
 extends Control
+
+
+const QUALITY = "rendering/quality/"
+
+var list : Array = ProjectSettings.get_property_list()
+
+
+func _high_selected() -> void :
+	pass
+
+func _ready() -> void :
+	var graphics = $LodTransition2/GraphicsOptions
+	graphics.connect("low_selected", self, "_low_selected")
+	graphics.connect("high_selected", self, "_high_selected")
+
+#Set the graphics settings as low as possible.
+func _low_selected() -> void :
+	_pj_set(QUALITY + "intended_usage/framebuffer_allocation", 3)
+	
+	var text : String = QUALITY + "filters/"
+	_pj_set(text + "anisotropic_filter_level", 1)
+	_pj_set(text+"use_nearest_mipmap_filter", false)
+	_pj_set(text+"msaa", 0)
+	
+	text = QUALITY+"directional_shadow/"
+	_pj_set(text+"size", 256)
+	
+	text = QUALITY+"shadow_atlas/quadrant_"
+	_pj_set(text+"0_subdiv", 0)
+	_pj_set(text+"1_subdiv", 0)
+	_pj_set(text+"2_subdiv", 0)
+	_pj_set(text+"3_subdiv", 0)
+	text = QUALITY+"shadow_atlas/"
+	_pj_set(text+"size", 256)
+	
+	text = QUALITY + "shadows/"
+	_pj_set(text+"filter_mode", 0)
+	
+	text = QUALITY + "reflections/"
+	_pj_set(text+"texture_array_reflections", false)
+	_pj_set(text+"high_quality_ggx", false)
+	_pj_set(text+"irradiance_max_size", 32)
+	_pj_set(text+"atlas_size", 0)
+	_pj_set(text+"atlas_subdiv", 0)
+	
+	text = QUALITY+"shading/"
+	_pj_set(text+"force_vertex_shading", true)
+	_pj_set(text+"force_lambert_over_burley", true)
+	
+	text = QUALITY+"depth_prepass/"
+	_pj_set(text+"enable", false)
+	
+	text = QUALITY+"subsurface_scattering/"
+	_pj_set(text+"quality", 0)
+	_pj_set(text+"scale", 0)
+	
+	text = QUALITY+"depth/"
+	_pj_set(text+"hdr", false)
+	
+
+#Helper function for setting ProjectSettings with less typing.
+func _pj_set(property : String, value) -> void :
+	assert(property in ProjectSettings)
+	ProjectSettings.set(property, value)
