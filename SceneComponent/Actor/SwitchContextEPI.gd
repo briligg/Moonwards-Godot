a1eefcb0364ae1c130f3b94526c49aed228bd1f4
Zach777 <MeteorCity3109@protonmail.com>
2021-06-05 10:16:13 -0400

    Fill out SwitchContextEPI

--- SceneComponent/Actor/SwitchContextEPI.gd
+++ SceneComponent/Actor/SwitchContextEPI.gd
@@ -1,6 +1,17 @@
 extends EPIBase
 class_name SwitchContextEPI
 
+#Make a don't actually change variable for dummy scripts.
+export(bool) var is_dummy : bool = false
 
-func take_context_from(_old_context : SwitchContextEPI) -> void :
-	pass
+signal context_lost()
+const CONTEXT_LOST = "context_lost"
+signal context_taken()
+const CONTEXT_TAKEN = "context_taken"
+
+func take_context_from(old_context : SwitchContextEPI = null) -> void :
+	if old_context == null :
+		return
+	
+	old_context.emit_signal(old_context.CONTEXT_LOST)
+	emit_signal(CONTEXT_TAKEN)
--- Script/Manager/EPIManager/EPIManager.gd
+++ Script/Manager/EPIManager/EPIManager.gd
@@ -2,6 +2,8 @@ extends Node
 
 const INPUT_EPI : String = "InputEPI"
 
+const SWITCH_CONTEXT_EPI = "SwitchContextEPI"
+
 func get_dummy_epi(epi_name : String) -> EPIBase :
 	assert(has_node(epi_name))
 	var epi : EPIBase = get_node(epi_name)
