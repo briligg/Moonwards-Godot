8d356bd3baac19d49060864f4d18d1449744c141
Zach777 <MeteorCity3109@protonmail.com>
2021-06-23 07:25:25 -0400

    Add request, assume and demand setter functions

--- SceneComponent/Actor/ModelEPI.gd
+++ SceneComponent/Actor/ModelEPI.gd
@@ -1,7 +1,11 @@
 extends EPIBase
 class_name ModelEPI
 
+signal field_set(field_name_string, value_variable)
+const FIELD_SET : String = "field_set"
 
+#What all fields there are. Fields names are written as strings like so: CamelCaseVariableName_Type
+#Underscore should only be for differentiating the variable name from it's declared type.
 export(PoolStringArray) var fields : PoolStringArray = PoolStringArray()
 var field_data_array : Array = []
 
@@ -10,6 +14,7 @@ func _ready() -> void :
 	for field in fields :
 		field_data_array.append(_get_default_type(field))
 
+#Get what type of variable a field is based on it's field string.
 func _get_default_type(field_name : String) :
 	var check : String = field_name.right(field_name.find("_", 0) + 1)
 	if check == "Color" :
@@ -23,3 +28,44 @@ func _get_default_type(field_name : String) :
 		Log.critical(self, "_get_default_type", "Field entry %s in epi %s is invalid." % [field_name, get_path() ])
 		assert(true == false)
 		return
+
+#Try to set a field but only give an error if the field is not present.
+func assume_set_field(field_name : String, value) -> void :
+	var at : int = _find_field(field_name)
+	if at == - 1 :
+		Log.error(self, "assume_set_field", "Tried to assign %s field to value %s in %s but field was not there" % [field_name, str(value), get_path()])
+		return
+	
+	#We do have the field so set it to the passed value argument.
+	field_data_array[at] = value
+	emit_signal(FIELD_SET, value)
+
+#For when that field must be present for everything to work correctly. Crash if field is not present.
+func demand_set_field(field_name : String, value) -> void :
+	var at : int = _find_field(field_name)
+	if at == - 1 :
+		Log.critical(self, "demand_set_field", "%s had the field %s demanded to be set but field was not there" % [get_path(), field_name])
+		assert(true == false) #Make sure to crash when debugging.
+		return
+	
+	#We have the field so set it to the passed value argument.
+	field_data_array[at] = value
+	emit_signal(FIELD_SET, value)
+
+#Intended for calling outside the entity. Does not emit an error or crash if the field is not present.
+func request_set_field(field_name : String, value) -> void :
+	var at : int = _find_field(field_name)
+	if at == -1 :
+		return
+	
+	#We do have the field so set it to the passed value argument.
+	field_data_array[at] = value
+	emit_signal(FIELD_SET, value)
+
+func _find_field(field_name) -> int :
+	var at : int = 0
+	for field in fields :
+		if field == field_name :
+			return at
+		at += 1
+	return -1
